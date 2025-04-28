9a1d7c7a53ff6d052e6b57b133a71483ae705723
moshaosama <mohamedOSFekry@gmail.com>
2025-04-28 19:29:50 +0300

    feat: update Sidebar index

--- src/Components/Sidebar/index.tsx
+++ src/Components/Sidebar/index.tsx
@@ -1,9 +1,46 @@
+import { FaPersonCircleCheck } from "react-icons/fa6";
+import { LiaTasksSolid } from "react-icons/lia";
+import {
+  MdAccountBalance,
+  MdPersonSearch,
+  MdOutlinePermContactCalendar,
+  MdOutlineLibraryBooks,
+} from "react-icons/md";
+import { PiUsersThreeBold } from "react-icons/pi";
+import { RiGovernmentFill } from "react-icons/ri";
 import { TbCircleLetterMFilled } from "react-icons/tb";
+import { NavLink } from "react-router";
 
 const Sidebar = () => {
   return (
-    <div className="my-5">
+    <div className="my-5 flex flex-col items-center gap-10">
       <TbCircleLetterMFilled className="text-white text-3xl" />
+      <div className="text-white text-xl flex flex-col gap-7 ">
+        <NavLink to={"/"}>
+          <PiUsersThreeBold />
+        </NavLink>
+        <NavLink to={"/accounts"}>
+          <MdAccountBalance />
+        </NavLink>
+        <NavLink to={"/recruiters"}>
+          <MdPersonSearch />
+        </NavLink>
+        <NavLink to={"/contacts"}>
+          <MdOutlinePermContactCalendar />
+        </NavLink>
+        <NavLink to={"/candidates"}>
+          <FaPersonCircleCheck />
+        </NavLink>
+        <NavLink to={"/companies"}>
+          <RiGovernmentFill />
+        </NavLink>
+        <NavLink to={"/roles"}>
+          <MdOutlineLibraryBooks />
+        </NavLink>
+        <NavLink to={"/tasks"}>
+          <LiaTasksSolid />
+        </NavLink>
+      </div>
     </div>
   );
 };
