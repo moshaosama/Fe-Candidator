c10bc2fa04101fd7d57dfa0767ac93824f3d0916
moshaosama <mohamedOSFekry@gmail.com>
2025-04-28 19:34:47 +0300

    feat: update AddModal component

--- src/Components/AddModal.tsx
+++ src/Components/AddModal.tsx
@@ -4,7 +4,7 @@ interface AddMoodalProps {
 
 const AddModal = ({ Title }: AddMoodalProps) => {
   return (
-    <button className="bg-purple-700 rounded-xl hover:bg-purple-500 transition-all duration-300 text-white w-20 h-9 cursor-pointer">
+    <button className="bg-purple-700 rounded-xl hover:bg-purple-500 transition-all duration-300 text-white w-fit px-2 h-9 cursor-pointer">
       {Title}
     </button>
   );
