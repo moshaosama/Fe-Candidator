ed23a67d9500b4ea32838b834cf57717ac297919
moshaosama <mohamedOSFekry@gmail.com>
2025-04-28 19:40:45 +0300

    feat: update Dashboard component

--- src/Components/Dashboard/index.tsx
+++ src/Components/Dashboard/index.tsx
@@ -1,10 +1,21 @@
+import { useEffect, useState } from "react";
 import { MdKeyboardDoubleArrowRight } from "react-icons/md";
+import { useLocation } from "react-router";
 
 const Dashboard = () => {
+  const { pathname } = useLocation();
+  const [pathName, setPathName] = useState(pathname);
+
+  useEffect(() => {
+    setPathName(pathname);
+  }, [pathname]);
   return (
-    <div className="my-5 flex items-center gap-2 px-3 text-white text-3xl">
+    <div className="my-5 flex items-center justify-center gap-2 px-3 text-white text-3xl">
       <MdKeyboardDoubleArrowRight />
       <hr className="h-7 border-gray-500 border-1" />
+      <h1 className="text-[19px] font-bold" key={pathname}>
+        {pathName.slice(1)}
+      </h1>
     </div>
   );
 };
