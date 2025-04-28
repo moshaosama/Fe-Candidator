904de286bb462cea60408024fe5fdd0651b546b0
moshaosama <mohamedOSFekry@gmail.com>
2025-04-28 22:37:34 +0300

    feat: update SplitLayout component

--- src/Layout/SplitLayout/index.tsx
+++ src/Layout/SplitLayout/index.tsx
@@ -1,20 +1,24 @@
+import { ReactNode } from "react";
 import { Outlet } from "react-router";
 
 interface SplitLayoutProps {
-  children: any;
+  children: [ReactNode, ReactNode];
 }
 
 const SplitLayout = ({ children }: SplitLayoutProps) => {
-  const [Left, Right] = children;
+  const [Dashboard, Sidebar] = children;
 
   return (
     <div className="flex gap-10">
-      <div className="w-14 border-white border-1 h-[50pc] mx-3 my-5 flex justify-center rounded-2xl bg-gradient-to-b from-purple-700 to-10%">
-        {Right}
+      <div
+        className="w-20 transition-all duration-700 border-white border-1 h-[50pc] mx-3 my-5 flex justify-center rounded-2xl bg-gradient-to-b from-purple-700 to-10%"
+        id="Sidebar"
+      >
+        {Sidebar}
       </div>
       <div className="flex flex-col w-full mr-20">
         <div className="rounded-2xl my-5 bg-gradient-to-r h-fit from-purple-700 to-50% border-1 border-white flex">
-          {Left}
+          {Dashboard}
         </div>
         <Outlet />
       </div>
