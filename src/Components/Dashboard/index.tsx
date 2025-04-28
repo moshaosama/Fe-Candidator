f16c8bc394b8647ccc4149a785f7345c638e243e
moshaosama <mohamedOSFekry@gmail.com>
2025-04-28 22:37:34 +0300

    feat: update Dashboard component

--- src/Components/Dashboard/index.tsx
+++ src/Components/Dashboard/index.tsx
@@ -1,17 +1,34 @@
 import { useEffect, useState } from "react";
-import { MdKeyboardDoubleArrowRight } from "react-icons/md";
 import { useLocation } from "react-router";
+import OpenSidebarFactory from "../../Utils/OpenSidebarFactory";
+import useDashboardInstructions from "./Hook/useDashboardInstructions";
 
 const Dashboard = () => {
   const { pathname } = useLocation();
   const [pathName, setPathName] = useState(pathname);
+  const { Active, handleClickActive } = useDashboardInstructions();
+
+  const SidebarElement = document.getElementById("Sidebar");
+  useEffect(() => {
+    const GetSidebarElement = async () => {
+      try {
+        if (SidebarElement) {
+          console.log(SidebarElement);
+        }
+      } catch (err) {
+        return err;
+      }
+    };
+
+    GetSidebarElement();
+  }, []);
 
   useEffect(() => {
     setPathName(pathname);
   }, [pathname]);
   return (
     <div className="my-5 flex items-center justify-center gap-2 px-3 text-white text-3xl">
-      <MdKeyboardDoubleArrowRight />
+      <OpenSidebarFactory Type={Active} onClick={handleClickActive} />
       <hr className="h-7 border-gray-500 border-1" />
       <h1 className="text-[19px] font-bold" key={pathname}>
         {pathName.slice(1)}
