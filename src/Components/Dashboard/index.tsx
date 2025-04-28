904f08588ce7172914b9b9751f37234baf43b0f3
moshaosama <mohamedOSFekry@gmail.com>
2025-04-28 18:18:06 +0300

    feat: add new Components

---
+++ src/Components/Dashboard/index.tsx
@@ -0,0 +1,11 @@
+import { TbCircleLetterMFilled } from "react-icons/tb";
+
+const Dashboard = () => {
+  return (
+    <div className="my-5">
+      <TbCircleLetterMFilled className="text-white text-3xl" />
+    </div>
+  );
+};
+
+export default Dashboard;
---
+++ src/Components/Sidebar/index.tsx
@@ -0,0 +1,11 @@
+import { TbCircleLetterMFilled } from "react-icons/tb";
+
+const Sidebar = () => {
+  return (
+    <div className="my-5">
+      <TbCircleLetterMFilled className="text-white text-3xl" />
+    </div>
+  );
+};
+
+export default Sidebar;
