ef67b36c0b8d7f0900a38d239152f24d9e02dacd
moshaosama <mohamedOSFekry@gmail.com>
2025-04-29 18:17:01 +0300

    feat: update useDashboardInstructions hook

--- src/Components/Dashboard/Hook/useDashboardInstructions.tsx
+++ src/Components/Dashboard/Hook/useDashboardInstructions.tsx
@@ -19,6 +19,8 @@ const updateSidebarStyles = (
 const useDashboardInstructions = () => {
   const [Active, setActive] = useState<ActiveSidebar>(ActiveSidebar.UnActive);
   const SidebarElement = document.getElementById("Sidebar");
+  const SidebarLabels = document.querySelectorAll(".Label") as any;
+
   const handleClickActive = async () => {
     setActive(
       Active === ActiveSidebar.Active
@@ -29,6 +31,7 @@ const useDashboardInstructions = () => {
     try {
       if (Active == ActiveSidebar.Active) {
         updateSidebarStyles(SidebarElement, "15pc", "10px", "start");
+        SidebarLabels.style.setProperty("display", "flex");
       } else {
         updateSidebarStyles(SidebarElement, "5pc", "0px", "center");
       }
