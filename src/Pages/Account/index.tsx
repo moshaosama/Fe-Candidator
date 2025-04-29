dcae3889e66d4ff341cbc39e6f39281a364e7940
moshaosama <mohamedOSFekry@gmail.com>
2025-04-29 18:17:01 +0300

    feat: update Account page

--- src/Pages/Account/index.tsx
+++ src/Pages/Account/index.tsx
@@ -1,9 +1,14 @@
 import SearchBar from "../../Components/SearchBar";
+import AddAccountModel from "../../features/Account/Components/AddAccountModel";
+import useAddAccountModel from "../../features/Account/Hooks/useAddAccountModel";
 
 const Accounts = () => {
+  const { handleOpenModel, OpenModel } = useAddAccountModel();
+
   return (
     <>
-      <SearchBar Title="Add Account" />
+      <SearchBar Title="Add Account" handleClick={handleOpenModel} />
+      {OpenModel && <AddAccountModel handleClick={handleOpenModel} />}
     </>
   );
 };
