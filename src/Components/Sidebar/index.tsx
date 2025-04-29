bde397bf0a1e874c4fa1faf0ba0bdc8683606a9a
moshaosama <mohamedOSFekry@gmail.com>
2025-04-29 18:17:01 +0300

    feat: update Sidebar component

--- src/Components/Sidebar/index.tsx
+++ src/Components/Sidebar/index.tsx
@@ -16,29 +16,58 @@ const Sidebar = () => {
     <div className="my-5 flex flex-col items-center gap-10">
       <TbCircleLetterMFilled className="text-white text-3xl" />
       <div className="text-white text-xl flex flex-col gap-7 ">
-        <NavLink to={"/"}>
+        <NavLink to={"/"} className="flex items-center gap-3 cursor-pointer">
           <PiUsersThreeBold />
+          <label className="Label">Users</label>
         </NavLink>
-        <NavLink to={"/accounts"}>
+        <NavLink
+          to={"/accounts"}
+          className="flex items-center gap-3 cursor-pointer"
+        >
           <MdAccountBalance />
+          <label className="Label">Accounts</label>
         </NavLink>
-        <NavLink to={"/recruiters"}>
+        <NavLink
+          to={"/recruiters"}
+          className="flex items-center gap-3 cursor-pointer"
+        >
           <MdPersonSearch />
+          <label className="Label">Recruiters</label>
         </NavLink>
-        <NavLink to={"/contacts"}>
+        <NavLink
+          to={"/contacts"}
+          className="flex items-center gap-3 cursor-pointer"
+        >
           <MdOutlinePermContactCalendar />
+          <label className="Label">Contacts</label>
         </NavLink>
-        <NavLink to={"/candidates"}>
+        <NavLink
+          to={"/candidates"}
+          className="flex items-center gap-3 cursor-pointer"
+        >
           <FaPersonCircleCheck />
+          <label className="Label">Candidates</label>
         </NavLink>
-        <NavLink to={"/companies"}>
+        <NavLink
+          to={"/companies"}
+          className="flex items-center gap-3 cursor-pointer"
+        >
           <RiGovernmentFill />
+          <label className="Label">Company</label>
         </NavLink>
-        <NavLink to={"/roles"}>
+        <NavLink
+          to={"/roles"}
+          className="flex items-center gap-3 cursor-pointer"
+        >
           <MdOutlineLibraryBooks />
+          <label className="Label">Roles</label>
         </NavLink>
-        <NavLink to={"/tasks"}>
+        <NavLink
+          to={"/tasks"}
+          className="flex items-center gap-3 cursor-pointer"
+        >
           <LiaTasksSolid />
+          <label className="Label">Tasks</label>
         </NavLink>
       </div>
     </div>
