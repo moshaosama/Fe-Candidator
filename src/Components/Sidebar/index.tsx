cac04d06ca8e58289d1c61000fa7316325358c0e
moshaosama <mohamedOSFekry@gmail.com>
2025-05-01 11:18:54 +0300

    style: adjust Sidebar layout and styling

--- src/Components/Sidebar/index.tsx
+++ src/Components/Sidebar/index.tsx
@@ -10,64 +10,83 @@ import { PiUsersThreeBold } from "react-icons/pi";
 import { RiGovernmentFill } from "react-icons/ri";
 import { TbCircleLetterMFilled } from "react-icons/tb";
 import { NavLink } from "react-router";
+import { clsx } from "clsx";
+import { useDashboardContext } from "../../Context/DashboardContext";
 
 const Sidebar = () => {
+  const { Active } = useDashboardContext();
   return (
     <div className="my-5 flex flex-col items-center gap-10">
       <TbCircleLetterMFilled className="text-white text-3xl" />
       <div className="text-white text-xl flex flex-col gap-7 ">
         <NavLink to={"/"} className="flex items-center gap-3 cursor-pointer">
           <PiUsersThreeBold />
-          <label className="Label">Users</label>
+          <label className={clsx(Active == "Active" ? "" : "hidden")}>
+            Users
+          </label>
         </NavLink>
         <NavLink
           to={"/accounts"}
           className="flex items-center gap-3 cursor-pointer"
         >
           <MdAccountBalance />
-          <label className="Label">Accounts</label>
+          <label className={clsx(Active == "Active" ? "" : "hidden")}>
+            Accounts
+          </label>
         </NavLink>
         <NavLink
           to={"/recruiters"}
           className="flex items-center gap-3 cursor-pointer"
         >
           <MdPersonSearch />
-          <label className="Label">Recruiters</label>
+          <label className={clsx(Active == "Active" ? "" : "hidden")}>
+            Recruiters
+          </label>
         </NavLink>
         <NavLink
           to={"/contacts"}
           className="flex items-center gap-3 cursor-pointer"
         >
           <MdOutlinePermContactCalendar />
-          <label className="Label">Contacts</label>
+          <label className={clsx(Active == "Active" ? "" : "hidden")}>
+            Contacts
+          </label>
         </NavLink>
         <NavLink
           to={"/candidates"}
           className="flex items-center gap-3 cursor-pointer"
         >
           <FaPersonCircleCheck />
-          <label className="Label">Candidates</label>
+          <label className={clsx(Active == "Active" ? "" : "hidden")}>
+            Candidates
+          </label>
         </NavLink>
         <NavLink
           to={"/companies"}
           className="flex items-center gap-3 cursor-pointer"
         >
           <RiGovernmentFill />
-          <label className="Label">Company</label>
+          <label className={clsx(Active == "Active" ? "" : "hidden")}>
+            Company
+          </label>
         </NavLink>
         <NavLink
           to={"/roles"}
           className="flex items-center gap-3 cursor-pointer"
         >
           <MdOutlineLibraryBooks />
-          <label className="Label">Roles</label>
+          <label className={clsx(Active == "Active" ? "" : "hidden")}>
+            Roles
+          </label>
         </NavLink>
         <NavLink
           to={"/tasks"}
           className="flex items-center gap-3 cursor-pointer"
         >
           <LiaTasksSolid />
-          <label className="Label">Tasks</label>
+          <label className={clsx(Active == "Active" ? "" : "hidden")}>
+            Tasks
+          </label>
         </NavLink>
       </div>
     </div>
