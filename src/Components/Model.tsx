25bf18218b44acdae926facacf9ffb116329cac1
moshaosama <mohamedOSFekry@gmail.com>
2025-05-01 17:21:14 +0300

    chore: update Model component implementation

--- src/Components/Model.tsx
+++ src/Components/Model.tsx
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { ModelProps } from "../Types/ModelType";
+import clsk from "clsx";
 
 const Model = ({ children, Header }: ModelProps) => {
+  const [Open, setOpen] = useState(false);
+  const Model = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    const handleOpen = async () => {
+      try {
+        if (Model.current) {
+          setOpen(true);
+        }
+      } catch (err) {
+        throw new Error(err as string);
+      }
+    };
+    handleOpen();
+  }, [Open]);
   return (
     <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center bg-[#030303b3]">
-      <div className="bg-gradient-to-b from-orange-950 from-5% to-indigo-950 to-95% w-[35pc] rounded-lg p-3 shadow-lg  shadow-orange-950">
+      <div
+        className={clsk(
+          "bg-gradient-to-b  from-orange-950 from-5% to-indigo-950 to-95% w-[35pc] rounded-lg p-3 shadow-lg  shadow-orange-950 transition-all duration-600",
+          Open
+            ? "opacity-100 scale-100 max-h-fit"
+            : "opacity-0 scale-95 max-h-0"
+        )}
+        ref={Model}
+      >
         <div>
           <h1 className="text-md font-bold text-white">{Header}</h1>
           {React.Children.map(children, () => {
