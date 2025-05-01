558ba143e2fff9687b6b60399ffd3d9a8f4d1bb3
moshaosama <mohamedOSFekry@gmail.com>
2025-05-01 17:08:56 +0300

    chore: update Users page component

--- src/Pages/Users/index.tsx
+++ src/Pages/Users/index.tsx
@@ -1,9 +1,13 @@
 import SearchBar from "../../Components/SearchBar";
+import AddUserModel from "../../features/Users/Components/AddUserModel";
+import { userAddUserModelContext } from "../../features/Users/Context/AddUserModelConext";
 
 const Users = () => {
+  const { AddModel, handleClickAddModel } = userAddUserModelContext();
   return (
     <>
-      <SearchBar Title="Add User" />
+      <SearchBar Title="Add User" handleClick={handleClickAddModel} />
+      {AddModel && <AddUserModel />}
     </>
   );
 };
