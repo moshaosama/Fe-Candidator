2d284c08176222c26b4175ef2a191e1fc1702ad6
moshaosama <mohamedOSFekry@gmail.com>
2025-05-02 01:35:41 +0300

    fix: update Users page layout and logic

--- src/Pages/Users/index.tsx
+++ src/Pages/Users/index.tsx
@@ -1,5 +1,6 @@
 import SearchBar from "../../Components/SearchBar";
 import AddUserModel from "../../features/Users/Components/AddUserModel";
+import LabelUserData from "../../features/Users/Components/LabelUserData";
 import { userAddUserModelContext } from "../../features/Users/Context/AddUserModelConext";
 
 const Users = () => {
@@ -8,6 +9,7 @@ const Users = () => {
     <>
       <SearchBar Title="Add User" handleClick={handleClickAddModel} />
       {AddModel && <AddUserModel />}
+      <LabelUserData />
     </>
   );
 };
