2d1d177e982da4fad553917af1f8433acdb59a63
moshaosama <mohamedOSFekry@gmail.com>
2025-05-08 12:09:26 +0300

    Added src/stories directory

---
+++ src/stories/Stories/LabelData.stories.tsx
@@ -0,0 +1,33 @@
+import LabelData from "../../Components/LabelData";
+
+const meta =  {
+    title: "Data/LabelData",
+    component: LabelData,
+
+    argTypes: {
+        color: { control: "color" }
+    }
+}
+
+
+export default meta;
+
+
+
+export const DisplayData = {
+    args: {
+        labels: [
+            {name: "First Name"},
+            {name: "Last Name"},
+            {name: "Email"},
+            {name: "Role"},
+            {name: "Status"},
+            {name: "Action"},
+        ],
+        color: "red",
+        display: "grid",
+        gridTemplateColumns: "repeat(6, 1fr)",
+        gap: "10px",
+        margin:"10px"
+    }
+}
