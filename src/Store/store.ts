702f2bdac877acf94e4d8944c5b7f92e13914ffa
moshaosama <mohamedOSFekry@gmail.com>
2025-05-08 14:30:54 +0300

    Update: Store configuration changes

--- src/Store/store.ts
+++ src/Store/store.ts
@@ -1,10 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-
+import RecruiterReducer from "../features/Recruiters/Reducer/Recruiter"
 
 
 export const store = configureStore({
     reducer: {
-
+        recruiter: RecruiterReducer
     }
 })
 
