a20e7e8a49678687c76ba5a1da8cd4cee66f764c
moshaosama <mohamedOSFekry@gmail.com>
2025-05-08 19:28:01 +0300

    Update: Enhanced SplitLayout with layout adjustments

--- src/Layout/SplitLayout/index.tsx
+++ src/Layout/SplitLayout/index.tsx
@@ -2,6 +2,7 @@ import { ReactNode } from "react";
 import { Outlet } from "react-router";
 import { clsx } from "clsx";
 import { useDashboardContext } from "../../Components/Dashboard/Context/DashboardContext";
+import SidebarResponsive from "../../Components/Sidebar/SidebarResponsive";
 
 interface SplitLayoutProps {
   children: [ReactNode, ReactNode];
@@ -15,17 +16,23 @@ const SplitLayout = ({ children }: SplitLayoutProps) => {
     <div className="flex gap-10">
       <div
         className={clsx(
-          "transition-all duration-700 border-white border-1 h-[50pc] mx-3 my-5 flex justify-center rounded-2xl bg-gradient-to-b from-purple-700 to-10%",
+          "transition-all max-sm:hidden duration-700 border-white border-1 h-[50pc] mx-3 my-5 flex justify-center rounded-2xl bg-gradient-to-b from-purple-700 to-10%",
           Active === "Active" ? "w-56" : "w-20"
         )}
       >
         {Sidebar}
       </div>
+
+      <div className="sm:hidden absolute">
+        <SidebarResponsive/>
+      </div>
       <div className="flex flex-col w-full mr-20">
-        <div className="rounded-2xl my-5 bg-gradient-to-r h-fit from-purple-700 to-50% border-1 border-white flex">
+        <div className="rounded-2xl my-5 max-sm:w-[43vh] max-sm:mx-2 bg-gradient-to-r h-fit from-purple-700 to-50% border-1 border-white flex">
           {Dashboard}
         </div>
-        <Outlet />
+        <div className="max-sm:mx-2">
+          <Outlet />
+        </div>
       </div>
     </div>
   );
