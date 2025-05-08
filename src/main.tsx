cd77e3f5484a329ccb7d692859840753de7d83c0
moshaosama <mohamedOSFekry@gmail.com>
2025-05-08 13:41:04 +0300

    Modified main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import DashboardProvider from "./Components/Dashboard/Context/DashboardContext.tsx";
-
+import { store } from "./Store/store.ts";
+import { Provider } from "react-redux";
 createRoot(document.getElementById("root")!).render(
   <DashboardProvider>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </DashboardProvider>
 );
