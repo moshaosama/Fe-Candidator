c13b6b86d5d56c6f0b6c7074f8ee9584318bf6e4
moshaosama <mohamedOSFekry@gmail.com>
2025-05-08 15:23:56 +0300

    Update: Modify DisplayData component for recruiter data

--- src/features/Recruiters/Components/DisplayData.tsx
+++ src/features/Recruiters/Components/DisplayData.tsx
@@ -12,11 +12,14 @@ const DisplayData = () => {
         recruiter.map((recruiter: RecruiterData, index: number) => (
             <div className=" bg-[#272727ab] mt-3 py-5 px-4 rounded-lg cursor-pointer">
                 <div key={index} className="grid grid-cols-4 text-white">
-                    <h1>{recruiter.FirstName}</h1>
+                    <h1 className="flex items-center gap-2">
+                        <input id="Data" type="checkbox" className="w-4 h-4 rounded-full" />
+                        {recruiter.FirstName}
+                    </h1>
                     <h1>{recruiter.LastName}</h1>
                     <h1>{recruiter.Email}</h1>
                     <h1>{recruiter.Role}</h1>
-            </div>
+               </div>
             </div>
         ))
        }
