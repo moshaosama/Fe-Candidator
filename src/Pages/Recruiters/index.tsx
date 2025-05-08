8d6d082338f12e009f714148c616c099bd59c01d
moshaosama <mohamedOSFekry@gmail.com>
2025-05-08 12:40:08 +0300

    Modified src/Pages/Recruiters/index.tsx

--- src/Pages/Recruiters/index.tsx
+++ src/Pages/Recruiters/index.tsx
@@ -1,9 +1,15 @@
 import SearchBar from "../../Components/SearchBar";
+import AddRecruitwerModal from "../../features/Recruiters/Components/AddRecruitwerModal";
+import LabelRecruiterData from "../../features/Recruiters/Components/LabelRecruiterData";
+import { useAddRecruiterModalContext } from "../../features/Recruiters/Context/AddRecruiterModalContext";
 
 const Recruiters = () => {
+  const {AddModal, handleClickAddModel} = useAddRecruiterModalContext();
   return (
     <>
-      <SearchBar Title="Add Recruiter" />
+      <SearchBar Title="Add Recruiter" handleClick={handleClickAddModel} />
+      <LabelRecruiterData />
+      {AddModal && <AddRecruitwerModal/>}
     </>
   );
 };
