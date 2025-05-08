3e64c4829549411712f929b911863e7dfb6483ba
moshaosama <mohamedOSFekry@gmail.com>
2025-05-08 14:31:02 +0300

    Update: Recruiter label component layout

--- src/features/Recruiters/Components/LabelRecruiterData.tsx
+++ src/features/Recruiters/Components/LabelRecruiterData.tsx
@@ -1,12 +1,14 @@
 
 import LabelData from "../../../Components/LabelData";
 import { RecruiterData } from "../Data/Data";
+import DisplayData from "./DisplayData";
 
 
 const LabelRecruiterData = () => {
     return (
         <div>
             <LabelData labels={RecruiterData}/>
+            <DisplayData/>
         </div>
     )
 }
