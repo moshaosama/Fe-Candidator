fe126dbb48d70bad7afbcef2b720313a9f2c9da6
moshaosama <mohamedOSFekry@gmail.com>
2025-05-08 15:01:31 +0300

    Update: Refactor form handling hook

--- src/features/Recruiters/Hooks/useFormData.ts
+++ src/features/Recruiters/Hooks/useFormData.ts
@@ -3,15 +3,18 @@ import { useDispatch } from "react-redux";
 import { AppDispatch } from "../../../Store/store";
 import { fetchCreateRecruiter } from "../Actions/createRecruiter";
 import { fetchGetRecruiter } from "../Actions/getRecruiters";
+import { useAddRecruiterModalContext } from "../Context/AddRecruiterModalContext";
 
 const useFormData = () => {
     const {register, handleSubmit, formState: {errors}} = useForm();
+    const {handleClickAddModel} = useAddRecruiterModalContext()
     const dispatch = useDispatch<AppDispatch>()
 
 
     const handleFormSubmit = async (data: any) => {
         await dispatch(fetchCreateRecruiter(data));
         dispatch(fetchGetRecruiter());
+        handleClickAddModel();
     }
 
     return {register, handleSubmit, errors, handleFormSubmit}
