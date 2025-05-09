4bfe226261c0e09ac43ba0b88387fdfc213ba56c
moshaosama <mohamedOSFekry@gmail.com>
2025-05-10 00:10:38 +0300

    Latest Versiob

--- src/features/Recruiters/Actions/createRecruiter.ts
+++ src/features/Recruiters/Actions/createRecruiter.ts
@@ -8,7 +8,7 @@ import axios from 'axios'
 
 export const fetchCreateRecruiter = createAsyncThunk("recruiter/createRecruiter", async (data: createRecruiterData, thunkAPI) => {
     try {
-        await axios.post("http://localhost:8000/create-recruiter", data);
+        await axios.post("https://be-candidator-production.up.railway.app/create-recruiter", data);
     } catch (error) {
         return thunkAPI.rejectWithValue(error)
     }
--- src/features/Recruiters/Actions/getRecruiters.ts
+++ src/features/Recruiters/Actions/getRecruiters.ts
@@ -4,7 +4,7 @@ import axios from "axios";
 export const fetchGetRecruiter = createAsyncThunk("recruiter/getRecruiter", async (_, thunkAPI) => {
     try {
         
-        const response = await axios.get("http://localhost:8000/get-recruiter")
+        const response = await axios.get("https://be-candidator-production.up.railway.app/get-recruiter")
         return response.data;
     } catch (error) {
         return thunkAPI.rejectWithValue(error)
