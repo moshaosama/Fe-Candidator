3d1f0fb3bbaa0149b71a685b891d4a1744062baf
moshaosama <mohamedOSFekry@gmail.com>
2025-05-11 01:09:04 +0300

    Update Recruiter reducer logic

--- src/features/Recruiters/Reducer/Recruiter.ts
+++ src/features/Recruiters/Reducer/Recruiter.ts
@@ -1,43 +1,52 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { fetchCreateRecruiter } from "../Actions/createRecruiter";
 import { fetchGetRecruiter } from "../Actions/getRecruiters";
+import { fetchDeleteRecruiterById } from "../Actions/deleteRecruiterbyid";
 
 const initialState = {
-    recruiter: [],
-    isLoading: false,
-    error: ""
-}
-
+  recruiter: [],
+  isLoading: false,
+  error: "",
+};
 
 const recruiterSlice = createSlice({
-    name: "recruiter",
-    initialState,
-    reducers: {},
-    extraReducers: (builder) => {
-        builder.addCase(fetchCreateRecruiter.pending, (state) => {
-            state.isLoading = true;
-        })
-        builder.addCase(fetchCreateRecruiter.fulfilled, (state) => {
-            state.isLoading = false;
-        })
-        builder.addCase(fetchCreateRecruiter.rejected, (state, action) => {
-            state.isLoading = false;
-            state.error = action.payload as string;
-        })
+  name: "recruiter",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchCreateRecruiter.pending, (state) => {
+      state.isLoading = true;
+    });
+    builder.addCase(fetchCreateRecruiter.fulfilled, (state) => {
+      state.isLoading = false;
+    });
+    builder.addCase(fetchCreateRecruiter.rejected, (state, action) => {
+      state.isLoading = false;
+      state.error = action.payload as string;
+    });
 
-        builder.addCase(fetchGetRecruiter.pending, (state) => {
-            state.isLoading = true;
-        })
-        builder.addCase(fetchGetRecruiter.fulfilled, (state, action) => {
-            state.isLoading = false;
-            state.recruiter = action.payload;
-        })
-        builder.addCase(fetchGetRecruiter.rejected, (state, action) => {
-            state.isLoading = false;
-            state.error = action.payload as string;
-        })
-    }
-})
-
+    builder.addCase(fetchGetRecruiter.pending, (state) => {
+      state.isLoading = true;
+    });
+    builder.addCase(fetchGetRecruiter.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.recruiter = action.payload;
+    });
+    builder.addCase(fetchGetRecruiter.rejected, (state, action) => {
+      state.isLoading = false;
+      state.error = action.payload as string;
+    });
+    builder.addCase(fetchDeleteRecruiterById.pending, (state) => {
+      state.isLoading = true;
+    });
+    builder.addCase(fetchDeleteRecruiterById.fulfilled, (state) => {
+      state.isLoading = false;
+    });
+    builder.addCase(fetchDeleteRecruiterById.rejected, (state, action) => {
+      state.isLoading = false;
+      state.error = action.payload as string;
+    });
+  },
+});
 
 export default recruiterSlice.reducer;
