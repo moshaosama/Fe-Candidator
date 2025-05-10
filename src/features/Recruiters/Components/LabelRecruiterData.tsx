6d7886a54e4bf9bef0a374b0bab43462ebe8762e
moshaosama <mohamedOSFekry@gmail.com>
2025-05-11 01:09:04 +0300

    Refactor LabelRecruiterData component

--- src/features/Recruiters/Components/LabelRecruiterData.tsx
+++ src/features/Recruiters/Components/LabelRecruiterData.tsx
@@ -1,29 +1,28 @@
-
 import LabelData from "../../../Components/LabelData";
 import NoData from "../../../Components/NoData";
 import { RecruiterData } from "../Data/Data";
 import useDisplayData from "../Hooks/useDisplayData";
 import DisplayData from "./DisplayData";
 
-
-const LabelRecruiterData = () => {
-    const {recruiter} = useDisplayData();
-    return (
-        <>
-        {
-            recruiter.length > 0 ? (
-                <div>
-                    <LabelData labels={RecruiterData}/>
-                    <DisplayData/>
-                </div>
-            ) : (
-               <NoData Title="recruiters"/>
-            )
-        }
-        </>
-    )
+interface LabelRecruiterDataProps {
+  checked: boolean;
+  handleChecked: () => void;
 }
 
-
+const LabelRecruiterData = ({ handleChecked }: LabelRecruiterDataProps) => {
+  const { recruiter } = useDisplayData();
+  return (
+    <>
+      {recruiter.length > 0 ? (
+        <div>
+          <LabelData labels={RecruiterData} />
+          <DisplayData handleChecked={handleChecked} />
+        </div>
+      ) : (
+        <NoData Title="recruiters" />
+      )}
+    </>
+  );
+};
 
 export default LabelRecruiterData;
