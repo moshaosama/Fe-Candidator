8f0ea2536cf9fece0890e38c819e416d29accf8e
moshaosama <mohamedOSFekry@gmail.com>
2025-05-11 01:09:05 +0300

    Add action to delete recruiter by ID

---
+++ src/features/Recruiters/Actions/deleteRecruiterbyid.ts
@@ -0,0 +1,16 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const fetchDeleteRecruiterById = createAsyncThunk(
+  "recruiter/deleteRecruiterById",
+  async (id: number, thunkAPI: any) => {
+    try {
+      const response = await axios.delete(
+        `https://be-candidator-production.up.railway.app/delete-recruiter-by-id/${id}`
+      );
+      return response.data;
+    } catch (err) {
+      return thunkAPI.rejectWithValue(err as string);
+    }
+  }
+);
