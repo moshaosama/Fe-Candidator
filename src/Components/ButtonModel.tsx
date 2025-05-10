17396a28ab32a08b082eee6b455da934416f7a0d
moshaosama <mohamedOSFekry@gmail.com>
2025-05-11 01:16:51 +0300

    Update ButtonModel component logic or UI

--- src/Components/ButtonModel.tsx
+++ src/Components/ButtonModel.tsx
@@ -6,7 +6,7 @@ interface ButtonModelProps {
 const ButtonModel = ({ Title, handleClick }: ButtonModelProps) => {
   return (
     <button
-      className="bg-purple-700 rounded-xl hover:bg-purple-500 transition-all duration-300 text-white w-fit px-2 h-9 cursor-pointer"
+      className="bg-purple-700 rounded-xl hover:bg-purple-500 max-sm:text-sm transition-all duration-300 text-white w-fit px-2 h-9 cursor-pointer"
       onClick={handleClick}
     >
       {Title}
