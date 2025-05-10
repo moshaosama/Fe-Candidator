1e085c31bdf100c1c0247561eeb4595d37460551
moshaosama <mohamedOSFekry@gmail.com>
2025-05-11 01:09:02 +0300

    Enhance SearchBar functionality or UI

--- src/Components/SearchBar.tsx
+++ src/Components/SearchBar.tsx
@@ -1,11 +1,19 @@
 import ButtonModel from "./ButtonModel";
+import DeleteModal from "./DeleteModal";
 
 interface SearchBarProps {
   Title: string;
+  isDelete?: boolean;
   handleClick?: () => void;
+  handleDelete: () => void;
 }
 
-const SearchBar = ({ Title, handleClick }: SearchBarProps) => {
+const SearchBar = ({
+  Title,
+  handleClick,
+  isDelete,
+  handleDelete,
+}: SearchBarProps) => {
   return (
     <div className="flex justify-between">
       <input
@@ -13,7 +21,10 @@ const SearchBar = ({ Title, handleClick }: SearchBarProps) => {
         className="border-gray-700 w-52 border-1 h-10 rounded-xl text-gray-400 text-sm px-2"
         placeholder="Search"
       />
-      <ButtonModel Title={Title} handleClick={handleClick} />
+      <div className="flex gap-2 items-center">
+        <ButtonModel Title={Title} handleClick={handleClick} />
+        {isDelete ? <DeleteModal handleDelete={handleDelete!} /> : null}
+      </div>
     </div>
   );
 };
