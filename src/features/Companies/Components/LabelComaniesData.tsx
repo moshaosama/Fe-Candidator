eeaab69e34fc8cae88dfd747159ad644c101d5c7
moshaosama <mohamedOSFekry@gmail.com>
2025-05-12 20:40:02 +0300

    Add new Companies feature with related components

---
+++ src/features/Companies/Components/LabelComaniesData.tsx
@@ -0,0 +1,12 @@
+import LabelData from "../../../Components/LabelData";
+import { LabelDataCompany } from "../Data/LabelData";
+
+const LabelComaniesData = () => {
+  return (
+    <>
+      <LabelData labels={LabelDataCompany} />
+    </>
+  );
+};
+
+export default LabelComaniesData;
---
+++ src/features/Companies/Data/LabelData.ts
@@ -0,0 +1,14 @@
+export const LabelDataCompany = [
+  {
+    name: "Name",
+  },
+  {
+    name: "Language",
+  },
+  {
+    name: "Jobs",
+  },
+  {
+    name: "Tags",
+  },
+];
