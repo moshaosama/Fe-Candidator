29a63a8283ba6c5c3de27c6c43ff5e68a4c9ea26
moshaosama <mohamedOSFekry@gmail.com>
2025-05-12 22:29:23 +0300

    Fix typos and improve rendering in LabelComaniesData component

--- src/features/Companies/Components/LabelComaniesData.tsx
+++ src/features/Companies/Components/LabelComaniesData.tsx
@@ -1,10 +1,12 @@
 import LabelData from "../../../Components/LabelData";
 import { LabelDataCompany } from "../Data/LabelData";
+import DisplayData from "./DisplayData";
 
 const LabelComaniesData = () => {
   return (
     <>
       <LabelData labels={LabelDataCompany} />
+      <DisplayData />
     </>
   );
 };
