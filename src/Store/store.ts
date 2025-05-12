9f73ca83aae054cc9e55f324ce8c124c59fc11e2
moshaosama <mohamedOSFekry@gmail.com>
2025-05-12 22:29:22 +0300

    Update store to register Companies reducer

--- src/Store/store.ts
+++ src/Store/store.ts
@@ -1,14 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
-import RecruiterReducer from "../features/Recruiters/Reducer/Recruiter"
-
+import RecruiterReducer from "../features/Recruiters/Reducer/Recruiter";
+import CompanyReducer from "../features/Companies/Reducer/CompanyReducer";
 
 export const store = configureStore({
-    reducer: {
-        recruiter: RecruiterReducer
-    }
-})
+  reducer: {
+    recruiter: RecruiterReducer,
+    company: CompanyReducer,
+  },
+});
 
-
-
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
