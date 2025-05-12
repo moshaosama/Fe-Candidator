8dad27ebb23452cf92f90e8911ba93aa4ab6c0a4
moshaosama <mohamedOSFekry@gmail.com>
2025-05-12 22:29:29 +0300

    Add type definitions for Companies feature

---
+++ src/features/Companies/Types/CompanyType.ts
@@ -0,0 +1,21 @@
+export interface CreateCompanyData {
+  FirstName: string;
+  LastName: string;
+  Email: string;
+  Password: string;
+  Language: string;
+}
+
+export interface CompanyData {
+  company: {
+    result: {
+      Email: string;
+      Name: string;
+      Language: string;
+      Jobs: {}[];
+      Tags: string;
+    }[];
+  };
+  isLoading: boolean;
+  error: string;
+}
