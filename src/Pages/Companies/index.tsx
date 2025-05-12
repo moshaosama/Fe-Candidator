21886cab1a2daf58035ad5ee430d06037f1bdb7f
moshaosama <mohamedOSFekry@gmail.com>
2025-05-12 22:29:22 +0300

    Update Companies index page with new layout and logic

--- src/Pages/Companies/index.tsx
+++ src/Pages/Companies/index.tsx
@@ -1,13 +1,22 @@
+import NoData from "../../Components/NoData";
 import SearchBar from "../../Components/SearchBar";
 import AddCompanyModal from "../../features/Companies/Components/AddCompanyModal";
 import LabelComaniesData from "../../features/Companies/Components/LabelComaniesData";
+import { useAddCompanyModalContext } from "../../features/Companies/Context/AddModalCompanyContext";
+import useGetCompanyData from "../../features/Companies/Hooks/useGetCompanyData";
 
 const Companies = () => {
+  const { handleTriggerModal, isOpen } = useAddCompanyModalContext();
+  const { Company } = useGetCompanyData();
   return (
     <>
-      <SearchBar Title="Add Company" />
-      <LabelComaniesData />
-      <AddCompanyModal />
+      <SearchBar Title="Add Company" handleClick={handleTriggerModal} />
+      {Company?.company?.result?.length === 0 ? (
+        <NoData Title="Company" />
+      ) : (
+        <LabelComaniesData />
+      )}
+      {isOpen && <AddCompanyModal />}
     </>
   );
 };
