508890219e4d11cca6e5ffa334a54837b1d13b2d
moshaosama <mohamedOSFekry@gmail.com>
2025-05-15 00:13:59 +0300

    Enhance Header component

--- src/features/Jobs/Components/Header.tsx
+++ src/features/Jobs/Components/Header.tsx
@@ -1,7 +1,19 @@
+import UseGetCompanyByID from "../Hooks/UseGetCompanyByID";
+
 const Header = () => {
+  const { CompanyByID } = UseGetCompanyByID();
+  //   console.log(CompanyByID.company.result?.[0]?.Name);
   return (
     <div className="mt-16">
-      <h1 className="text-white text-3xl font-bold">Jobs for:</h1>
+      <h1 className="text-white text-3xl font-bold">
+        Jobs for:{" "}
+        <span className="text-purple-700 font-bold text-3xl">
+          {CompanyByID.company.result?.[0]?.Name
+            ? CompanyByID.company.result?.[0]?.Name
+            : "Loading..."}
+        </span>
+      </h1>
+      <hr className="mt-5  text-gray-500" />
     </div>
   );
 };
