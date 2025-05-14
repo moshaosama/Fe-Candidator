018d297936f75991966de9b37aa7f55254406e50
moshaosama <mohamedOSFekry@gmail.com>
2025-05-15 02:10:00 +0300

    chore: updated Redux store

--- src/Store/store.ts
+++ src/Store/store.ts
@@ -1,10 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import RecruiterReducer from "../features/Recruiters/Reducer/Recruiter";
 import CompanyReducer from "../features/Companies/Reducer/CompanyReducer";
+import ContactReducer from "../features/Contact/Reducer/ContactReducer";
+
 export const store = configureStore({
   reducer: {
     recruiter: RecruiterReducer,
     company: CompanyReducer,
+    contact: ContactReducer,
   },
 });
 
