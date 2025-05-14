8b991ab52a287c6ae1e5fae43aaef0dbd3171b68
moshaosama <mohamedOSFekry@gmail.com>
2025-05-14 23:57:44 +0300

    Refactor DisplayData component

--- src/features/Companies/Components/DisplayData.tsx
+++ src/features/Companies/Components/DisplayData.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router";
 import useGetCompanyData from "../Hooks/useGetCompanyData";
 
 const DisplayData = () => {
@@ -6,20 +7,19 @@ const DisplayData = () => {
   return (
     <>
       {Company?.company.result?.map((Company, index) => (
-        <div
-          className="bg-[#272727ab] mt-3 py-5 px-4 rounded-lg cursor-pointer"
-          key={index}
-        >
-          <div
-            key={index}
-            className="grid grid-cols-4 text-white font-semibold max-sm:text-[10px]"
-          >
-            <h1>{Company.Name}</h1>
-            <h1>{Company.Language}</h1>
-            <h1>{Company.Jobs?.length > 0 ? Company.Jobs?.length : 0}</h1>
-            <h1>{Company.Tags ? Company.Tags : "-"}</h1>
+        <Link key={index} to={`/companies/${Company.id}`}>
+          <div className="bg-[#272727ab] mt-3 py-5 px-4 rounded-lg cursor-pointer">
+            <div
+              key={index}
+              className="grid grid-cols-4 text-white font-semibold max-sm:text-[10px]"
+            >
+              <h1>{Company.Name}</h1>
+              <h1>{Company.Language}</h1>
+              <h1>{Company.Jobs?.length > 0 ? Company.Jobs?.length : 0}</h1>
+              <h1>{Company.Tags ? Company.Tags : "-"}</h1>
+            </div>
           </div>
-        </div>
+        </Link>
       ))}
     </>
   );
