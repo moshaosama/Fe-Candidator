2b2bec30ccf4a1369b4bfcd1ce69d14b53120679
moshaosama <mohamedOSFekry@gmail.com>
2025-05-15 00:47:51 +0300

    Update Jobs index page

--- src/Pages/Jobs/index.tsx
+++ src/Pages/Jobs/index.tsx
@@ -1,13 +1,18 @@
 import SearchBar from "../../Components/SearchBar";
+import AddModelJob from "../../features/Jobs/Components/AddModelJob";
 import Header from "../../features/Jobs/Components/Header";
-
+import LabelJobData from "../../features/Jobs/Components/LabelJobData";
+import { useAddOpenModelJobContext } from "../../features/Jobs/Context/useAddOpenModelContext";
 const Jobs = () => {
+  const { openModel, handleOpenModel } = useAddOpenModelJobContext();
   return (
     <>
       <Header />
       <div className="mt-5">
-        <SearchBar Title="Add Job" />
+        <SearchBar Title="Add Job" handleClick={handleOpenModel} />
       </div>
+      <LabelJobData />
+      {openModel && <AddModelJob />}
     </>
   );
 };
