0babba83c05fead4c5c7d0d2a3b0ab9c7baae9e2
moshaosama <mohamedOSFekry@gmail.com>
2025-05-14 23:45:40 +0300

    Refactor useFormData hook

--- src/features/Companies/Hooks/useFormData.tsx
+++ src/features/Companies/Hooks/useFormData.tsx
@@ -1,4 +1,8 @@
 import { useForm } from "react-hook-form";
+import { useDispatch } from "react-redux";
+import { AppDispatch } from "../../../Store/store";
+import { fetchCreateCompany } from "../Actions/CreateCompany";
+import { useAddCompanyModalContext } from "../Context/AddModalCompanyContext";
 
 const useFormData = () => {
   const {
@@ -6,10 +10,12 @@ const useFormData = () => {
     handleSubmit,
     formState: { errors },
   } = useForm();
+  const dispatch = useDispatch<AppDispatch>();
+  const { handleTriggerModal } = useAddCompanyModalContext();
 
-  const onSubmit = (data: any, e: any) => {
-    e.preventDefault();
-    console.log(data);
+  const onSubmit = (data: any) => {
+    dispatch(fetchCreateCompany(data));
+    handleTriggerModal();
   };
 
   return { register, handleSubmit, errors, onSubmit };
