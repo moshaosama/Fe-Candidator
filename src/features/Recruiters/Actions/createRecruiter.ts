68208541f10de791f97cb3a55ce9ce819225cceb
moshaosama <mohamedOSFekry@gmail.com>
2025-05-14 23:15:14 +0300

    Update createRecruiter action with better request structure

--- src/features/Recruiters/Actions/createRecruiter.ts
+++ src/features/Recruiters/Actions/createRecruiter.ts
@@ -1,15 +1,18 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { createRecruiterData } from "../Types/createRecruiterType";
-import axios from 'axios'
+import axios from "axios";
 
-
-
-
-
-export const fetchCreateRecruiter = createAsyncThunk("recruiter/createRecruiter", async (data: createRecruiterData, thunkAPI) => {
+export const fetchCreateRecruiter = createAsyncThunk(
+  "recruiter/createRecruiter",
+  async (data: createRecruiterData, thunkAPI) => {
     try {
-        await axios.post("https://be-candidator-production.up.railway.app/create-recruiter", data);
+      await axios.post(
+        // "https://be-candidator-production.up.railway.app/create-recruiter",
+        "http://localhost:8000/create-recruiter",
+        data
+      );
     } catch (error) {
-        return thunkAPI.rejectWithValue(error)
+      return thunkAPI.rejectWithValue(error);
     }
-})
+  }
+);
