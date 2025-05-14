360561bb2dbfc571e794ffe73a8301e815b8eba6
moshaosama <mohamedOSFekry@gmail.com>
2025-05-14 23:57:47 +0300

    Add Jobs related components and features

---
+++ src/Pages/Jobs/index.tsx
@@ -0,0 +1,11 @@
+import Header from "../../features/Jobs/Components/Header";
+
+const Jobs = () => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
+
+export default Jobs;
---
+++ src/features/Jobs/Components/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div className="mt-16">
+      <h1 className="text-white text-3xl font-bold">Jobs for:</h1>
+    </div>
+  );
+};
+
+export default Header;
