0b345a95bb42aefc6f57872c5185a7cc7d36daa3
moshaosama <mohamedOSFekry@gmail.com>
2025-05-15 02:09:59 +0300

    fix: updated Contacts index page

--- src/Pages/Contacts/index.tsx
+++ src/Pages/Contacts/index.tsx
@@ -1,9 +1,15 @@
 import SearchBar from "../../Components/SearchBar";
+import AddContactModel from "../../features/Contact/Components/AddContactModel";
+import LabelContactData from "../../features/Contact/Components/LabelContactData";
+import { useAddContactModalContext } from "../../features/Contact/Context/AddContactModalContext";
 
 const Contacts = () => {
+  const { openModal, handleOpenModal } = useAddContactModalContext();
   return (
     <>
-      <SearchBar Title="Add Contact" />
+      <SearchBar Title="Add Contact" handleClick={handleOpenModal} />
+      <LabelContactData />
+      {openModal && <AddContactModel />}
     </>
   );
 };
