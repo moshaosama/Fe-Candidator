0a4caf02c9d791f2990103e9a7515abc30f73fd0
moshaosama <mohamedOSFekry@gmail.com>
2025-05-14 23:45:40 +0300

    Update CreateCompany action logic

--- src/features/Companies/Actions/CreateCompany.ts
+++ src/features/Companies/Actions/CreateCompany.ts
@@ -1,17 +1,26 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { CreateCompanyData } from "../Types/CompanyType";
-import axios from "axios";
 
 export const fetchCreateCompany = createAsyncThunk(
   "Company/fetchCreateCompany",
   async (data: CreateCompanyData, thunkAPI) => {
     try {
-      const response = await axios.post(
-        // "https://be-candidator-production.up.railway.app/create-company",
-        "http://localhost:8000/create-company",
-        data
-      );
-      return response.data;
+      const response = await fetch("http://localhost:8000/create-company", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+
+      if (!response.ok) {
+        const errorData = await response.json();
+        return thunkAPI.rejectWithValue(errorData.message || "Request failed");
+      }
+
+      const result = await response.json();
+
+      return result;
     } catch (error) {
       return thunkAPI.rejectWithValue(error as string);
     }
