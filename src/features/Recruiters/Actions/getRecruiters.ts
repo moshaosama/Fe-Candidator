1b27fe147ea8e266ec1804ffa4288bfb8e96f8f0
moshaosama <mohamedOSFekry@gmail.com>
2025-05-14 23:15:15 +0300

    Update getRecruiters action to support pagination and filtering

--- src/features/Recruiters/Actions/getRecruiters.ts
+++ src/features/Recruiters/Actions/getRecruiters.ts
@@ -1,12 +1,17 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-export const fetchGetRecruiter = createAsyncThunk("recruiter/getRecruiter", async (_, thunkAPI) => {
+export const fetchGetRecruiter = createAsyncThunk(
+  "recruiter/getRecruiter",
+  async (_, thunkAPI) => {
     try {
-        
-        const response = await axios.get("https://be-candidator-production.up.railway.app/get-recruiter")
-        return response.data;
+      const response = await axios.get(
+        // "https://be-candidator-production.up.railway.app/get-recruiter"
+        "http://localhost:8000/get-recruiter"
+      );
+      return response.data;
     } catch (error) {
-        return thunkAPI.rejectWithValue(error)
+      return thunkAPI.rejectWithValue(error);
     }
-})
+  }
+);
