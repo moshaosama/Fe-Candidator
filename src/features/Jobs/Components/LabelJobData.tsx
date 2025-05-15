b0a08d061f5c170a3903a04cf7818fa673a44de0
moshaosama <mohamedOSFekry@gmail.com>
2025-05-15 12:17:47 +0300

    feat(jobs): update job label data rendering

--- src/features/Jobs/Components/LabelJobData.tsx
+++ src/features/Jobs/Components/LabelJobData.tsx
@@ -1,8 +1,14 @@
 import LabelData from "../../../Components/LabelData";
 import { labelJobData } from "../Data/LabelJobdata";
+import DisplayData from "./DisplayData";
 
 const LabelJobData = () => {
-  return <LabelData labels={labelJobData} />;
+  return (
+    <>
+      <LabelData labels={labelJobData} />
+      <DisplayData />
+    </>
+  );
 };
 
 export default LabelJobData;
