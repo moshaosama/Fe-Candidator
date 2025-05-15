f6ee03feabd18e9fabd509e27535ae01e128f613
moshaosama <mohamedOSFekry@gmail.com>
2025-05-15 12:17:47 +0300

    chore(store): update store configuration for jobs and contact features

--- src/Store/store.ts
+++ src/Store/store.ts
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import RecruiterReducer from "../features/Recruiters/Reducer/Recruiter";
 import CompanyReducer from "../features/Companies/Reducer/CompanyReducer";
 import ContactReducer from "../features/Contact/Reducer/ContactReducer";
+import JobReducer from "../features/Jobs/Reducer/JobReducer";
 
 export const store = configureStore({
   reducer: {
     recruiter: RecruiterReducer,
     company: CompanyReducer,
     contact: ContactReducer,
+    job: JobReducer,
   },
 });
 
