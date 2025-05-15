cf319b28c17d9164a02fbacfac028006001a7bb9
moshaosama <mohamedOSFekry@gmail.com>
2025-05-15 12:17:55 +0300

    feat(jobs): add component to display job data

---
+++ src/features/Jobs/Components/DisplayData.tsx
@@ -0,0 +1,33 @@
+import useGetData from "../Hooks/useGetData";
+import { JobData } from "../Types/JobData";
+
+const DisplayData = () => {
+  const { Jobs } = useGetData();
+  console.log(Jobs);
+
+  return (
+    <>
+      {Jobs.jobs?.result?.map((job: JobData) => (
+        <div
+          key={job.id}
+          className="p-4 bg-[#272727ab] rounded-lg text-white font-semibold max-sm:text-[10px] cursor-pointer"
+        >
+          <div className="grid grid-cols-10  text-white font-semibold max-sm:text-[10px]">
+            <p>{job.JobTitle}</p>
+            <p>{job.Description}</p>
+            <p>{job.Department}</p>
+            <p>{job.NumNeeded}</p>
+            <p>-</p>
+            <p>{job.Location}</p>
+            <p>-</p>
+            <p>{job.Commitment}</p>
+            <p>-</p>
+            <p>{job.Contact}</p>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default DisplayData;
