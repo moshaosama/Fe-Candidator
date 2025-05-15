6332a12c625e262b1433f713817ce4d686793309
moshaosama <mohamedOSFekry@gmail.com>
2025-05-16 01:22:59 +0300

    feat: update job-related hooks for better data flow and reusability

--- src/features/Jobs/Hooks/useFormData.ts
+++ src/features/Jobs/Hooks/useFormData.ts
@@ -2,7 +2,7 @@ import { useForm } from "react-hook-form";
 import { useDispatch } from "react-redux";
 import { AppDispatch } from "../../../Store/store";
 import { fetchCreateJob } from "../Actions/CreateJob";
-
+import usetoastify from "../../../Hooks/usetoastify";
 const useFormData = () => {
   const {
     register,
@@ -10,9 +10,10 @@ const useFormData = () => {
     formState: { errors },
   } = useForm();
   const dispatch = useDispatch<AppDispatch>();
-
+  const { notifySuccess } = usetoastify();
   const onSubmit = (data: any) => {
     dispatch(fetchCreateJob(data));
+    notifySuccess("Job created successfully!!");
   };
   return { register, handleSubmit, errors, onSubmit };
 };
