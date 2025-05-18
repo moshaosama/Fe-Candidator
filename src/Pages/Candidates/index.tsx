7fa8f3cce654f08b3debfeb41674acd890c50152
moshaosama <mohamedOSFekry@gmail.com>
2025-05-19 01:57:34 +0300

    Update Candidates page (index.tsx)

--- src/Pages/Candidates/index.tsx
+++ src/Pages/Candidates/index.tsx
@@ -1,9 +1,18 @@
 import SearchBar from "../../Components/SearchBar";
-
+import { AddModalCandidator } from "../../features/Candidates/Components/AddModalCandidator";
+import LabelCandidateData from "../../features/Candidates/Components/LabelCandidateData";
+import { useOprenAddModalCandidator } from "../../features/Candidates/Context/OpenAddModalContext";
 const Candidates = () => {
+  const { openAddModalCandidator, handleOpenAddModalCandidator } =
+    useOprenAddModalCandidator();
   return (
     <>
-      <SearchBar Title="Add Candidate" />
+      <SearchBar
+        Title="Add Candidate"
+        handleClick={handleOpenAddModalCandidator}
+      />
+      <LabelCandidateData />
+      {openAddModalCandidator && <AddModalCandidator />}
     </>
   );
 };
