a53c5897f96779ba6f71afc0a948023d06c983d6
moshaosama <mohamedOSFekry@gmail.com>
2025-05-19 01:57:34 +0300

    Update store.ts

--- src/Store/store.ts
+++ src/Store/store.ts
@@ -3,13 +3,14 @@ import RecruiterReducer from "../features/Recruiters/Reducer/Recruiter";
 import CompanyReducer from "../features/Companies/Reducer/CompanyReducer";
 import ContactReducer from "../features/Contact/Reducer/ContactReducer";
 import JobReducer from "../features/Jobs/Reducer/JobReducer";
-
+import CandidatorReducer from "../features/Candidates/Reducer/Candidator";
 export const store = configureStore({
   reducer: {
     recruiter: RecruiterReducer,
     company: CompanyReducer,
     contact: ContactReducer,
     job: JobReducer,
+    candidator: CandidatorReducer,
   },
 });
 
