0439bedffd74adbbaf574f353f728380962a3465
moshaosama <mohamedOSFekry@gmail.com>
2025-05-19 16:09:57 +0300

    Update SplitLayout layout

--- src/Layout/SplitLayout/index.tsx
+++ src/Layout/SplitLayout/index.tsx
@@ -16,7 +16,7 @@ const SplitLayout = ({ children }: SplitLayoutProps) => {
     <div className="flex gap-10">
       <div
         className={clsx(
-          "transition-all max-sm:hidden duration-700 border-white border-1 h-[50pc] mx-3 my-5 flex justify-center rounded-2xl bg-gradient-to-b from-purple-700 to-10%",
+          "transition-all max-sm:hidden duration-700 border-white border-1 h-[95.5vh] mx-3 my-5 flex justify-center rounded-2xl bg-gradient-to-b from-purple-700 to-10%",
           Active === "Active" ? "w-56" : "w-20"
         )}
       >
@@ -24,7 +24,7 @@ const SplitLayout = ({ children }: SplitLayoutProps) => {
       </div>
 
       <div className="sm:hidden absolute">
-        <SidebarResponsive/>
+        <SidebarResponsive />
       </div>
       <div className="flex flex-col w-full mr-20">
         <div className="rounded-2xl my-5 max-sm:w-[50vh] max-sm:mx-2 bg-gradient-to-r h-fit from-purple-700 to-50% border-1 border-white flex">
