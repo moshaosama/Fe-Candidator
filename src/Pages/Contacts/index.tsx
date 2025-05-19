51c645226a5038db3c196a8b34ae02393d8cd308
moshaosama <mohamedOSFekry@gmail.com>
2025-05-19 12:42:08 +0300

    Update Contacts page

--- src/Pages/Contacts/index.tsx
+++ src/Pages/Contacts/index.tsx
@@ -17,3 +17,27 @@ const Contacts = () => {
 };
 
 export default Contacts;
+
+// import SearchBar from "../../Components/SearchBar";
+// import ToastNotify from "../../Components/ToastContainer";
+// import AddContactModel from "../../features/Contact/Components/AddContactModel";
+// import LabelContactData from "../../features/Contact/Components/LabelContactData";
+// import { useAddContactModalContext } from "../../features/Contact/Context/AddContactModalContext";
+// import useGetData from "../../features/Users/Hook/useGetData";
+// import { ISFoundData } from "../../Utils/ISFoundData";
+
+// const Contacts = () => {
+//   const { openModal, handleOpenModal } = useAddContactModalContext();
+//   const { Contact } = useGetData();
+//   const LabelData = ISFoundData(LabelContactData, "contact");
+//   return (
+//     <>
+//       <SearchBar Title="Add Contact" handleClick={handleOpenModal} />
+//       <LabelData Data={Contact} />
+//       {openModal && <AddContactModel />}
+//       <ToastNotify />
+//     </>
+//   );
+// };
+
+// export default Contacts;
