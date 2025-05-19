9daecf2779d516fbcfa8cb9c6ae090496db23f24
moshaosama <mohamedOSFekry@gmail.com>
2025-05-19 12:42:09 +0300

    Update useGetData hook

--- src/features/Jobs/Hooks/useGetData.ts
+++ src/features/Jobs/Hooks/useGetData.ts
@@ -3,19 +3,15 @@ import { AppDispatch, RootState } from "../../../Store/store";
 import { fetchGetContact } from "../../Contact/Actions/GetContact";
 import { useEffect } from "react";
 import { fetchGetJobs } from "../Actions/GetJobs";
-import usetoastify from "../../../Hooks/usetoastify";
 
 const useGetData = () => {
   const Contact: any = useSelector((state: RootState) => state.contact);
   const Jobs: any = useSelector((state: RootState) => state.job);
   const dispatch = useDispatch<AppDispatch>();
-  const { notifySuccess } = usetoastify();
 
   useEffect(() => {
     dispatch(fetchGetContact());
     dispatch(fetchGetJobs());
-    notifySuccess("Contact fetched successfully!!");
-    notifySuccess("Jobs fetched successfully!!");
   }, [dispatch]);
 
   return { Contact, Jobs };
