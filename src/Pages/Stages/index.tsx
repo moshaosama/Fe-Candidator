6d85f18168be5e0c14da4b716b68456a4d6e976f
moshaosama <mohamedOSFekry@gmail.com>
2025-05-20 02:08:54 +0300

    Update Stages index page

--- src/Pages/Stages/index.tsx
+++ src/Pages/Stages/index.tsx
@@ -2,20 +2,25 @@ import SearchBar from "../../Components/SearchBar";
 import Header from "../../features/Stages/Components/Header";
 import LabelStagesData from "../../features/Stages/Components/LabelStagesData";
 import StagesSection from "../../features/Stages/Components/StagesSection";
-
+import { useAddCandidateModelContext } from "../../features/Stages/Context/useAddCandidateModelContext";
+import AddModelCandidate from "../../features/Stages/Components/AddModelCandidate";
 const Stages = () => {
+  const { AddModal, handleClickAddModal } = useAddCandidateModelContext();
   return (
     <>
       <Header />
       <div className="mt-4">
         <SearchBar Title="Add Stages">
-          <h1 className="cursor-pointer">Add Candidate</h1>
+          <h1 className="cursor-pointer" onClick={handleClickAddModal}>
+            Add Candidate
+          </h1>
         </SearchBar>
       </div>
       <div className="flex gap-5 mt-3">
         <StagesSection />
         <LabelStagesData />
       </div>
+      {AddModal && <AddModelCandidate />}
     </>
   );
 };
