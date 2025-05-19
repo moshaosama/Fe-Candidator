9ccf01490bf7cddc87a960bf9b374199b1ec125d
moshaosama <mohamedOSFekry@gmail.com>
2025-05-19 12:42:08 +0300

    Update useGetCompanyData hook

--- src/features/Companies/Hooks/useGetCompanyData.tsx
+++ src/features/Companies/Hooks/useGetCompanyData.tsx
@@ -3,16 +3,14 @@ import { AppDispatch, RootState } from "../../../Store/store";
 import { useEffect } from "react";
 import { fetchGetCompanies } from "../Actions/GetCompanies";
 import { CompanyData } from "../Types/CompanyType";
-import usetoastify from "../../../Hooks/usetoastify";
+
 const useGetCompanyData = () => {
   const Company = useSelector(
     (state: RootState) => state.company
   ) as CompanyData;
   const dispatch = useDispatch<AppDispatch>();
-  const { notifySuccess } = usetoastify();
   useEffect(() => {
     dispatch(fetchGetCompanies());
-    notifySuccess("Company fetched successfully!!");
   }, [dispatch]);
 
   return { Company };
