ff4428ab619e5b9c09ea1bb9012a93ad8add9bc1
moshaosama <mohamedOSFekry@gmail.com>
2025-05-19 12:42:09 +0300

    Update useFormData hook

--- src/features/Contact/Hooks/useFormData.ts
+++ src/features/Contact/Hooks/useFormData.ts
@@ -2,7 +2,6 @@ import { useForm } from "react-hook-form";
 import { useDispatch } from "react-redux";
 import { AppDispatch } from "../../../Store/store";
 import { fetchCreateContact } from "../Actions/CreateContact";
-import usetoastify from "../../../Hooks/usetoastify";
 import { fetchGetContact } from "../Actions/GetContact";
 export const useFormData = () => {
   const {
@@ -11,11 +10,9 @@ export const useFormData = () => {
     formState: { errors },
   } = useForm();
   const dispatch = useDispatch<AppDispatch>();
-  const { notifySuccess } = usetoastify();
   const onSubmit = (data: any) => {
     dispatch(fetchCreateContact(data));
     dispatch(fetchGetContact());
-    notifySuccess("Contact created successfully!!");
   };
 
   return { register, handleSubmit, errors, onSubmit };
