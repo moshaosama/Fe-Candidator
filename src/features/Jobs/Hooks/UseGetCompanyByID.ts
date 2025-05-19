8148e4584a727f3886188a4a550687888540185f
moshaosama <mohamedOSFekry@gmail.com>
2025-05-19 13:42:08 +0300

    Update UseGetCompanyByID hook

--- src/features/Jobs/Hooks/UseGetCompanyByID.ts
+++ src/features/Jobs/Hooks/UseGetCompanyByID.ts
@@ -4,18 +4,16 @@ import { AppDispatch, RootState } from "../../../Store/store";
 import { useEffect } from "react";
 import { fetchGetCompanyByID } from "../Actions/GetCompanyByID";
 import { CompanyData } from "../../Companies/Types/CompanyType";
-import usetoastify from "../../../Hooks/usetoastify";
+
 const UseGetCompanyByID = () => {
   const { companyId } = useParams();
   const dispatch = useDispatch<AppDispatch>();
-  const { notifySuccess } = usetoastify();
   const CompanyByID = useSelector(
     (state: RootState) => state.company
   ) as CompanyData;
 
   useEffect(() => {
     dispatch(fetchGetCompanyByID(+companyId!));
-    notifySuccess("Company fetched successfully!!");
   }, [dispatch, companyId]);
 
   return { CompanyByID };
