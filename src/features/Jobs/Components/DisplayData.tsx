bfe5154a974b497fb5345d924f742ba92d679449
moshaosama <mohamedOSFekry@gmail.com>
2025-05-19 16:09:58 +0300

    Update Job DisplayData component

--- src/features/Jobs/Components/DisplayData.tsx
+++ src/features/Jobs/Components/DisplayData.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router";
 import { Company } from "../../Companies/Types/CompanyType";
 import UseGetCompanyByID from "../Hooks/UseGetCompanyByID";
 
@@ -5,8 +6,6 @@ import { JobData } from "../Types/JobData";
 
 const DisplayData = () => {
   const { CompanyByID } = UseGetCompanyByID();
-
-  console.log(CompanyByID);
 
   return (
     <>
@@ -25,23 +24,25 @@ const DisplayData = () => {
         return (
           <div key={company.id}>
             {jobs.map((job: JobData) => (
-              <div
-                key={job.id}
-                className="p-4 bg-[#272727ab] rounded-lg text-white font-semibold max-sm:text-[10px] cursor-pointer"
-              >
-                <div className="grid grid-cols-10  text-white font-semibold max-sm:text-[10px]">
-                  <p>{job.JobTitle}</p>
-                  <p>{job.Description}</p>
-                  <p>{job.Department}</p>
-                  <p>{job.NumNeeded}</p>
-                  <p>-</p>
-                  <p>{job.Location}</p>
-                  <p>-</p>
-                  <p>{job.Commitment}</p>
-                  <p>-</p>
-                  <p>{job.Contact}</p>
+              <Link to={`jobs/${job.id}`}>
+                <div
+                  key={job.id}
+                  className="p-4 bg-[#272727ab] rounded-lg text-white font-semibold max-sm:text-[10px] cursor-pointer"
+                >
+                  <div className="grid grid-cols-10  text-white font-semibold max-sm:text-[10px]">
+                    <p>{job.JobTitle}</p>
+                    <p>{job.Description}</p>
+                    <p>{job.Department}</p>
+                    <p>{job.NumNeeded}</p>
+                    <p>-</p>
+                    <p>{job.Location}</p>
+                    <p>-</p>
+                    <p>{job.Commitment}</p>
+                    <p>-</p>
+                    <p>{job.Contact}</p>
+                  </div>
                 </div>
-              </div>
+              </Link>
             ))}
           </div>
         );
