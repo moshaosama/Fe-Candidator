8b499f97506da799a72093c95d99445fbaf3ddc5
moshaosama <mohamedOSFekry@gmail.com>
2025-05-20 17:01:20 +0300

    Edit LabelStagesData: update layout and label management logic

--- src/features/Stages/Components/LabelStagesData.tsx
+++ src/features/Stages/Components/LabelStagesData.tsx
@@ -1,9 +1,11 @@
 import LabelData from "../../../Components/LabelData";
 import { StageData } from "../Data/StageData";
+import DisplayData from "./DisplayData";
 const LabelStagesData = () => {
   return (
     <div className="border-2 border-gray-500 w-full rounded-xl">
       <LabelData labels={StageData} />
+      <DisplayData />
     </div>
   );
 };
