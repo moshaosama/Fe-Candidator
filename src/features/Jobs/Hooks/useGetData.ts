bb55bd4207cc24f701bee2eb9dd166eb96483928
moshaosama <mohamedOSFekry@gmail.com>
2025-05-20 17:01:13 +0300

    Refactor useGetData: improve fetching and error handling

--- src/features/Jobs/Hooks/useGetData.ts
+++ src/features/Jobs/Hooks/useGetData.ts
@@ -3,11 +3,38 @@ import { AppDispatch, RootState } from "../../../Store/store";
 import { fetchGetContact } from "../../Contact/Actions/GetContact";
 import { useEffect } from "react";
 import { fetchGetJobs } from "../Actions/GetJobs";
-import UseGetCompanyByID from "./UseGetCompanyByID";
+
+interface JobsData {
+  error: string;
+  loading: boolean;
+  jobs: {
+    result: {
+      Candidates: {
+        FirstName: string;
+        LastName: string;
+        Email: string;
+        LinkedInProfile: string;
+        Stages: string;
+      }[];
+      Commitment: string;
+      Contact: string;
+      Department: string;
+      Description: string;
+      Gender: string;
+      JobTitle: string;
+      Location: string;
+      NumNeeded: number;
+      Stages: string;
+    }[];
+    success: boolean;
+  };
+}
 
 const useGetData = () => {
   const Contact: any = useSelector((state: RootState) => state.contact);
-  const Jobs: any = useSelector((state: RootState) => state.job);
+  const Jobs = useSelector(
+    (state: RootState) => state.job
+  ) as unknown as JobsData;
   const dispatch = useDispatch<AppDispatch>();
 
   useEffect(() => {
