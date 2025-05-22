ca27ebfa6df196cd3782452ca05220722d3af6c5
moshaosama <mohamedOSFekry@gmail.com>
2025-05-22 18:58:43 +0300

    Add SignUp feature module

---
+++ src/features/SignUp/Hooks/useFormData.ts
@@ -0,0 +1,18 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+
+const useFormData = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data: any) => {
+    console.log(data);
+  };
+
+  return { register, handleSubmit, errors, onSubmit };
+};
+
+export default useFormData;
