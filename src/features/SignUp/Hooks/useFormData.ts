b69b27b0643789609433cc7ed3619040a5ec5565
moshaosama <mohamedOSFekry@gmail.com>
2025-05-23 00:40:18 +0300

    Enhance useFormData logic for SignUp form

--- src/features/SignUp/Hooks/useFormData.ts
+++ src/features/SignUp/Hooks/useFormData.ts
@@ -1,4 +1,8 @@
 import { useForm } from "react-hook-form";
+import { useDispatch } from "react-redux";
+import { AppDispatch } from "../../../Store/store";
+import { fetchSignUp } from "../Action/SignUp";
+import { useHasAccountContext } from "../../../Context/useHasAccountContext";
 
 const useFormData = () => {
   const {
@@ -6,9 +10,12 @@ const useFormData = () => {
     handleSubmit,
     formState: { errors },
   } = useForm();
+  const dispatch = useDispatch<AppDispatch>();
+  const { toggleHasAccount } = useHasAccountContext();
 
   const onSubmit = (data: any) => {
-    console.log(data);
+    dispatch(fetchSignUp(data));
+    toggleHasAccount();
   };
 
   return { register, handleSubmit, errors, onSubmit };
