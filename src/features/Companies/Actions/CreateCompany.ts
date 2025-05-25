83e7d6b1153a616d7975186c0f704108e7c2056c
moshaosama <mohamedOSFekry@gmail.com>
2025-05-25 15:58:26 +0300

    Update CreateCompany action

--- src/features/Companies/Actions/CreateCompany.ts
+++ src/features/Companies/Actions/CreateCompany.ts
@@ -1,28 +1,10 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { CreateCompanyData } from "../Types/CompanyType";
+import { companyService } from "../../../APi/Company/CompanyService";
 
 export const fetchCreateCompany = createAsyncThunk(
   "Company/fetchCreateCompany",
   async (data: CreateCompanyData, thunkAPI) => {
-    try {
-      const response = await fetch("http://localhost:8000/create-company", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
-      });
-
-      if (!response.ok) {
-        const errorData = await response.json();
-        return thunkAPI.rejectWithValue(errorData.message || "Request failed");
-      }
-
-      const result = await response.json();
-
-      return result;
-    } catch (error) {
-      return thunkAPI.rejectWithValue(error as string);
-    }
+    return companyService.createNewData(data, thunkAPI);
   }
 );
