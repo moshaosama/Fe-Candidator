8a4709f77e0ca1fc98e0f089aa1e9f7792005672
moshaosama <mohamedOSFekry@gmail.com>
2025-05-26 20:37:34 +0300

    Update GetStages action

--- src/features/Stages/Actions/GetStages.ts
+++ src/features/Stages/Actions/GetStages.ts
@@ -1,14 +1,9 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import axios from "axios";
+import { stageService } from "../../../APi/Stages/stagesService";
 
 export const fetchGetStages = createAsyncThunk(
   "Stages/fetchGetStages",
-  async (_, { rejectWithValue }) => {
-    try {
-      const response = await axios.get("http://localhost:8000/get-stages");
-      return response.data;
-    } catch (error) {
-      return rejectWithValue(error as string);
-    }
+  async (_, thunkAPI) => {
+    return stageService.getAllData(thunkAPI);
   }
 );
