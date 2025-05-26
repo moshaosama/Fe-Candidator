f78b0598d3ccf3cf9cbabe36bf078e4c35788c29
moshaosama <mohamedOSFekry@gmail.com>
2025-05-26 22:20:28 +0300

    Update Candidates page index

--- src/Pages/Candidates/index.tsx
+++ src/Pages/Candidates/index.tsx
@@ -4,6 +4,7 @@ import { AddModalCandidator } from "../../features/Candidates/Components/AddModa
 import LabelCandidateData from "../../features/Candidates/Components/LabelCandidateData";
 import { useOprenAddModalCandidator } from "../../features/Candidates/Context/OpenAddModalContext";
 import { useGetCandidator } from "../../features/Candidates/Hooks/useGetCandidator";
+
 const Candidates = () => {
   const { openAddModalCandidator, handleOpenAddModalCandidator } =
     useOprenAddModalCandidator();
@@ -25,28 +26,3 @@ const Candidates = () => {
 };
 
 export default Candidates;
-
-// import SearchBar from "../../Components/SearchBar";
-// import { AddModalCandidator } from "../../features/Candidates/Components/AddModalCandidator";
-// import LabelCandidateData from "../../features/Candidates/Components/LabelCandidateData";
-// import { useOprenAddModalCandidator } from "../../features/Candidates/Context/OpenAddModalContext";
-// import { useGetCandidator } from "../../features/Candidates/Hooks/useGetCandidator";
-// import { ISFoundData } from "../../Utils/ISFoundData";
-// const Candidates = () => {
-//   const { openAddModalCandidator, handleOpenAddModalCandidator } =
-//     useOprenAddModalCandidator();
-//   const { Candidator } = useGetCandidator();
-//   const LabelData = ISFoundData(LabelCandidateData, "Candidator"); //HOC
-//   return (
-//     <>
-//       <SearchBar
-//         Title="Add Candidate"
-//         handleClick={handleOpenAddModalCandidator}
-//       />
-//       <LabelData Data={Candidator} />
-//       {openAddModalCandidator && <AddModalCandidator />}
-//     </>
-//   );
-// };
-
-// export default Candidates;
