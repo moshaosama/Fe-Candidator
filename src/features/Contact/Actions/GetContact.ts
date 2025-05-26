75d6f143ad2171591abcce179f4c69c8dc5e00c7
moshaosama <mohamedOSFekry@gmail.com>
2025-05-26 19:42:22 +0300

    Update GetContact action

--- src/features/Contact/Actions/GetContact.ts
+++ src/features/Contact/Actions/GetContact.ts
@@ -1,14 +1,9 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import axios from "axios";
+import { contactService } from "../../../APi/Contact/ContactService";
 
 export const fetchGetContact = createAsyncThunk(
   "Contact/fetchGetContact",
   async (_, thunkAPI) => {
-    try {
-      const response = await axios.get("http://localhost:8000/get-contacts");
-      return response.data;
-    } catch (error) {
-      return thunkAPI.rejectWithValue(error as string);
-    }
+    return contactService.getAllData(_, thunkAPI);
   }
 );
