b37f4e3505606507b73bb41685161dd3f7445b87
moshaosama <mohamedOSFekry@gmail.com>
2025-05-26 22:20:29 +0300

    Add CandidatorPage under Candidates page

---
+++ src/Pages/Candidates/CandidatorPage/index.tsx
@@ -0,0 +1,11 @@
+import Header from "../../../features/Candidates/Components/CandidatorPage/Header";
+
+const ViewCandidatorPage = () => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
+
+export default ViewCandidatorPage;
