5192a24f4a865e37f6e73c2e620c768182e25e63
moshaosama <mohamedOSFekry@gmail.com>
2025-05-26 20:37:34 +0300

    Update loginService.ts

--- src/APi/Login/loginService.ts
+++ src/APi/Login/loginService.ts
@@ -1,14 +1,10 @@
 import axios from "axios";
-import ParentService from "../ParentService";
 import { LoginData } from "../../features/Login/Action/Login";
 
-class LoginService extends ParentService {
-  createNewData(data: any, thunkAPI: any) {}
-  getAllData(_?: any, thunkAPI?: any) {}
-
+class LoginService {
   async Login(data: LoginData, thunkAPI: any) {
     try {
-      const response = await axios.post(`${this.url}/login`, data);
+      const response = await axios.post(`http://localhost:8000//login`, data);
       return response.data;
     } catch (err) {
       return thunkAPI.rejectWithValue(err as string);
