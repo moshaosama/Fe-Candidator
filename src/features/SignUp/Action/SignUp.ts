ddf8011cd2cb68909b06eb03e4274ab1193b8f32
moshaosama <mohamedOSFekry@gmail.com>
2025-05-26 20:37:34 +0300

    Update SignUp action

--- src/features/SignUp/Action/SignUp.ts
+++ src/features/SignUp/Action/SignUp.ts
@@ -1,6 +1,7 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import axios from "axios";
-interface SignUpData {
+import { signUpService } from "../../../APi/SignUp/SignUpService";
+
+export interface SignUpData {
   FirstName: string;
   LastName: string;
   Email: string;
@@ -9,12 +10,7 @@ interface SignUpData {
 
 export const fetchSignUp = createAsyncThunk(
   "SignUp/fetchSignUp",
-  async (data: SignUpData, { rejectWithValue }) => {
-    try {
-      const response = await axios.post("http://localhost:8000/sign-up", data);
-      return response.data;
-    } catch (err) {
-      return rejectWithValue(err as string);
-    }
+  async (data: SignUpData, thunkAPI) => {
+    return signUpService.SignUp(data, thunkAPI);
   }
 );
