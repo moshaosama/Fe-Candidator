56220db311d9f25007d4e214bf4268db2ed6099a
moshaosama <mohamedOSFekry@gmail.com>
2025-05-26 20:27:42 +0300

    Update getRecruiters action

--- src/features/Recruiters/Actions/getRecruiters.ts
+++ src/features/Recruiters/Actions/getRecruiters.ts
@@ -1,17 +1,8 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import axios from "axios";
-
+import { recruiterService } from "../../../APi/Recruiters/recruiterService";
 export const fetchGetRecruiter = createAsyncThunk(
   "recruiter/getRecruiter",
   async (_, thunkAPI) => {
-    try {
-      const response = await axios.get(
-        // "https://be-candidator-production.up.railway.app/get-recruiter"
-        "http://localhost:8000/get-recruiter"
-      );
-      return response.data;
-    } catch (error) {
-      return thunkAPI.rejectWithValue(error);
-    }
+    return recruiterService.getAllData(_, thunkAPI);
   }
 );
