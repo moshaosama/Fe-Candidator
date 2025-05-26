597a7157e20d3521f7896f396973eebc7289cc1e
moshaosama <mohamedOSFekry@gmail.com>
2025-05-26 23:46:09 +0300

    Add OverView section in CandidatorPage

---
+++ src/Pages/Candidates/CandidatorPage/OverView/index.tsx
@@ -0,0 +1,13 @@
+import HeaderOverView from "../../../../features/OverView/HeaderOverFlow";
+import { TasksOverFlow } from "../../../../features/OverView/TasksOverFlow";
+
+const OverView = () => {
+  return (
+    <>
+      <HeaderOverView />
+      <TasksOverFlow />
+    </>
+  );
+};
+
+export default OverView;
