bd47c54b858d849701081afc111dc891eff21f13
moshaosama <mohamedOSFekry@gmail.com>
2025-05-27 15:28:46 +0300

    Update HeaderJobCandidator component

--- src/features/JobsCandidatePage/HeaderJobCandidator.tsx
+++ src/features/JobsCandidatePage/HeaderJobCandidator.tsx
@@ -1,20 +1,8 @@
-import useGetData from "../Jobs/Hooks/useGetData";
-
 export const HeaderJobCandidator = () => {
-  const { Jobs } = useGetData();
-
-  console.log(Jobs.jobs?.result?.length);
-
   return (
     <div>
-      <div className="bg-[#432f52] w-full -mt-5 h-32 ">
-        <h1 className="text-4xl font-bold p-10">Jobs</h1>
-      </div>
-      <div className="mt-4 text-white p-10">
-        <h1 className="text-lg font-bold">
-          There are {Jobs.jobs?.result?.length} open jobs. To see relevant jobs
-          based on your profile
-        </h1>
+      <div className="bg-[#432f52] w-full -mt-5 h-32 flex items-center">
+        <h1 className="text-4xl font-bold ml-44">Jobs</h1>
       </div>
     </div>
   );
