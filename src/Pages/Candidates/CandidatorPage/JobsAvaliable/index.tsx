4ac54f777db28ceadcc02d4aa94e175d27939ba8
moshaosama <mohamedOSFekry@gmail.com>
2025-05-27 15:28:45 +0300

    Update JobsAvaliable index.tsx in CandidatorPage

--- src/Pages/Candidates/CandidatorPage/JobsAvaliable/index.tsx
+++ src/Pages/Candidates/CandidatorPage/JobsAvaliable/index.tsx
@@ -1,9 +1,13 @@
+import { DisplayJobData } from "../../../../features/JobsCandidatePage/DisplayJobData";
+import { Filteration } from "../../../../features/JobsCandidatePage/Filteration";
 import { HeaderJobCandidator } from "../../../../features/JobsCandidatePage/HeaderJobCandidator";
 
 export const JobsAvaliable = () => {
   return (
     <>
       <HeaderJobCandidator />
+      <Filteration />
+      <DisplayJobData />
     </>
   );
 };
