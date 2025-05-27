348a67008161b712c759abdef373a5a23679c302
moshaosama <mohamedOSFekry@gmail.com>
2025-05-27 15:28:45 +0300

    Update LayoutFactory utility

--- src/Utils/LayoutFactory.tsx
+++ src/Utils/LayoutFactory.tsx
@@ -4,19 +4,22 @@ import SplitLayout from "../Layout/SplitLayout";
 import HasAccountFactory from "./HasAccountFactory";
 import useGetPathName from "../Hooks/useGetIsRegister";
 import { useGetToken } from "../Hooks/useGetToken";
-import { useNavigate } from "react-router";
+import { useLocation, useNavigate } from "react-router";
 import { useEffect } from "react";
 import ViewCandidatorPage from "../Pages/Candidates/CandidatorPage";
 
 export const LayoutByRoleFactory = () => {
   const { User } = useGetToken();
   const { isRegister } = useGetPathName();
-  const Navigate = useNavigate();
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
 
   if (User?.result?.Role === "Candidate") {
     useEffect(() => {
-      Navigate("/careerhub/overview");
-    }, []);
+      if (User?.result?.Role === "Candidate" && pathname === "/") {
+        navigate("/careerhub/overview");
+      }
+    }, [User, pathname, navigate]);
     return <ViewCandidatorPage />;
   } else {
     {
