b09d87d64eef3914fc1a4b806ed566525daf4ede
moshaosama <mohamedOSFekry@gmail.com>
2025-05-29 02:22:22 +0300

    Update: Improved sidebar toggle logic

--- src/features/Candidates/Hooks/useOpenSidebar.ts
+++ src/features/Candidates/Hooks/useOpenSidebar.ts
@@ -2,15 +2,35 @@ import { useState } from "react";
 
 const useOpenSidebar = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [isOpenSideBarProfile, setIsOpenSideBarProfile] = useState(false);
 
   const handleTriggerOpenSidebar = () => {
     setIsOpen(!isOpen);
   };
+  const handleTriggerOpenSidebarProfile = () => {
+    setIsOpenSideBarProfile(!isOpenSideBarProfile);
+  };
   const handleCloseSidebar = () => {
     setIsOpen(false);
   };
+  const handleCloseSidebarProfile = () => {
+    setIsOpenSideBarProfile(false);
+  };
 
-  return { handleTriggerOpenSidebar, isOpen, handleCloseSidebar };
+  const handleCloseAll = () => {
+    handleCloseSidebar();
+    handleCloseSidebarProfile();
+  };
+
+  return {
+    handleTriggerOpenSidebar,
+    isOpen,
+    isOpenSideBarProfile,
+    handleCloseSidebar,
+    handleTriggerOpenSidebarProfile,
+    handleCloseSidebarProfile,
+    handleCloseAll,
+  };
 };
 
 export default useOpenSidebar;
