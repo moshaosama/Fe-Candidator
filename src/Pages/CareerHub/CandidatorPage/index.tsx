1c149bc1bee21ff61949bdfe728ed1e68e96f8f8
moshaosama <mohamedOSFekry@gmail.com>
2025-05-29 02:22:21 +0300

    Update: Updated CandidatorPage index layout and logic

--- src/Pages/CareerHub/CandidatorPage/index.tsx
+++ src/Pages/CareerHub/CandidatorPage/index.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router";
 import Header from "../../../features/Candidates/Components/CandidatorPage/Header";
+import { Footer } from "../../../features/Candidates/Components/CandidatorPage/Footer";
 
 const ViewCandidatorPage = () => {
   return (
     <>
       <Header />
       <Outlet />
+      <Footer />
     </>
   );
 };
