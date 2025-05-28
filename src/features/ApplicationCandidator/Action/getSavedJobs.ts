921748506a1ef0635916991b86ed47966240d118
moshaosama <mohamedOSFekry@gmail.com>
2025-05-28 15:18:16 +0300

    Add: Added actions for saving and fetching saved jobs

---
+++ src/features/ApplicationCandidator/Action/getSavedJobs.ts
@@ -0,0 +1,9 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { savedJobService } from "../../../APi/SavedJobs/SavedJobsService";
+
+export const fetchGetSavedJobs = createAsyncThunk(
+  "SavedJobs/fetchGetSavedJobs",
+  async (_, thunkAPI) => {
+    return savedJobService.getAllData(_, thunkAPI);
+  }
+);
