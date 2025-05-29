a9b5911c7d5d501af40d01a176bd91084a45a3b9
moshaosama <mohamedOSFekry@gmail.com>
2025-05-30 00:03:24 +0300

    Update useDetailsProfileFun hook

--- src/features/CandidateProfile/Hooks/useDetailsProfileFun.ts
+++ src/features/CandidateProfile/Hooks/useDetailsProfileFun.ts
@@ -1,24 +1,40 @@
 import { useState } from "react";
 import { useForm } from "react-hook-form";
+import { useDispatch } from "react-redux";
+import { AppDispatch } from "../../../Store/store";
+import { fetchEditProfile } from "../Actions/EditProfile";
+import { useGetToken } from "../../../Hooks/useGetToken";
+import { fetchGetCandidateByID } from "../../Candidates/Action/getCandidateById";
 
 export const useDetailsProfileFun = () => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpenDetailsProfile, setIsOpenDetailsProfile] = useState(false);
+  const { User } = useGetToken();
+
   const {
     register,
     formState: { errors },
     handleSubmit,
   } = useForm();
+  const dispatch = useDispatch<AppDispatch>();
 
   const handleTriggerOpen = () => {
-    setIsOpen(!isOpen);
+    setIsOpenDetailsProfile(!isOpenDetailsProfile);
   };
 
-  const onSubmit = (data: any) => {
-    console.log(data);
+  const onSubmit = async (data: any) => {
+    await dispatch(
+      fetchEditProfile({
+        candidateId: User?.result?.id,
+        data,
+      })
+    );
+    await dispatch(fetchGetCandidateByID(String(User?.result?.id)));
+
+    setIsOpenDetailsProfile(false);
   };
 
   return {
-    isOpen,
+    isOpenDetailsProfile,
     register,
     errors,
     handleSubmit,
