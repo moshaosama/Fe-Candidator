3dd63244f3e983ec9aa641eee5d1ed15315ea2f4
moshaosama <mohamedOSFekry@gmail.com>
2025-05-30 00:03:24 +0300

    Update EditProfile action

--- src/features/CandidateProfile/Actions/EditProfile.ts
+++ src/features/CandidateProfile/Actions/EditProfile.ts
@@ -1,9 +1,18 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { editProfileService } from "../../../APi/EditProfile/EditProfileService";
 
+interface EditProfileServiceData {
+  data: any;
+  candidateId: number;
+}
+
 export const fetchEditProfile = createAsyncThunk(
   "EditProfile/fetchCreateCandidator",
-  async (data: any, ThunkAPi) => {
-    return editProfileService.EditProfile(data, ThunkAPi);
+  async (data: EditProfileServiceData, ThunkAPi) => {
+    return editProfileService.EditProfile(
+      data.candidateId,
+      data.data,
+      ThunkAPi
+    );
   }
 );
