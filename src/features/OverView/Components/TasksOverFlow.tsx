014d69fe25c4ba619389327b68063c7c809f00ca
moshaosama <mohamedOSFekry@gmail.com>
2025-06-08 03:19:28 +0300

    modified: TasksOverFlow.tsx

--- src/features/OverView/Components/TasksOverFlow.tsx
+++ src/features/OverView/Components/TasksOverFlow.tsx
@@ -2,6 +2,7 @@ import { FaArrowRight } from "react-icons/fa6";
 import { useGetToken } from "../../../Hooks/useGetToken";
 import { Link } from "react-router";
 import useGetApplications from "../../ApplicationCandidator/Hooks/useGetApplications";
+import FavJobs from "./FavJobs";
 
 export const TasksOverFlow = () => {
   const { User } = useGetToken();
@@ -10,26 +11,31 @@ export const TasksOverFlow = () => {
   return (
     <>
       <div className="flex justify-between">
-        <div className="border-2 border-gray-400  p-5 my-3 mx-20 rounded-xl text-white w-96">
-          <h1 className="font-bold">Hi {User?.result?.FirstName}</h1>
-          <p>Welcome to your Career Hub!</p>
-          <hr className="my-3 bg-gray-500 h-1 border-none rounded-full" />
-          <div className="my-5  flex flex-col gap-5">
-            <h1 className="font-bold">My tasks</h1>
-            <div className="flex justify-between items-center">
-              <h1>Update your profile</h1>
-              <button className="border-2 rounded-full p-2 hover:bg-purple-500 cursor-pointer transition-all duration-300">
-                <FaArrowRight />
-              </button>
+        <div className="flex ">
+          <div className="border-2 border-gray-400  p-5 my-3 mx-20 rounded-xl text-white w-96">
+            <h1 className="font-bold">Hi {User?.result?.FirstName}</h1>
+            <p>Welcome to your Career Hub!</p>
+            <hr className="my-3 bg-gray-500 h-1 border-none rounded-full" />
+            <div className="my-5  flex flex-col gap-5">
+              <h1 className="font-bold">My tasks</h1>
+              <div className="flex justify-between items-center">
+                <h1>Update your profile</h1>
+                <button className="border-2 rounded-full p-2 hover:bg-purple-500 cursor-pointer transition-all duration-300">
+                  <FaArrowRight />
+                </button>
+              </div>
+            </div>
+            <hr className="my-3 bg-gray-500 h-1 border-none rounded-full" />
+            <div className="my-5  flex flex-col gap-5">
+              <h1 className="font-bold">My Activity</h1>
+              <div className="flex justify-between items-center cursor-pointer hover:underline">
+                <h1>Job Applications</h1>
+                <p>{result?.length}</p>
+              </div>
             </div>
           </div>
-          <hr className="my-3 bg-gray-500 h-1 border-none rounded-full" />
-          <div className="my-5  flex flex-col gap-5">
-            <h1 className="font-bold">My Activity</h1>
-            <div className="flex justify-between items-center cursor-pointer hover:underline">
-              <h1>Job Applications</h1>
-              <p>{result?.length}</p>
-            </div>
+          <div className="my-3">
+            <FavJobs />
           </div>
         </div>
         <Link
