34944789cb05876001a5038d7d08c5f5ee44c6ea
moshaosama <mohamedOSFekry@gmail.com>
2025-06-08 16:05:43 +0300

    modified: EditProfileService.ts

--- src/APi/EditProfile/EditProfileService.ts
+++ src/APi/EditProfile/EditProfileService.ts
@@ -1,11 +1,33 @@
 import axios from "axios";
+import ParentService from "../ParentService";
+import { CreateSkillData } from "../../Types/CreateSkill";
 
-class EditProfileService {
+class EditProfileService extends ParentService {
   async EditProfile(candidateId: number, data: any, thunkAPI: any) {
     try {
       const response = await axios.put(
-        `http://localhost:8000/edit-candidate-profile/${candidateId}`,
+        `${this.url}/edit-candidate-profile/${candidateId}`,
         data
+      );
+
+      return response.data;
+    } catch (err) {
+      return thunkAPI.rejectWithValue(err as string);
+    }
+  }
+  async createNewData(data: CreateSkillData, thunkAPI: any) {
+    try {
+      const response = await axios.post(`${this.url}/create-skill`, data);
+
+      return response.data;
+    } catch (err) {
+      return thunkAPI.rejectWithValue(err as string);
+    }
+  }
+  async getAllData(candidator_id?: number, thunkAPI?: any) {
+    try {
+      const response = await axios.get(
+        `${this.url}/get-skills/${candidator_id}`
       );
 
       return response.data;
