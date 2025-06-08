26e1a2c93890679c636084a1dc52e5c37bfd3c13
moshaosama <mohamedOSFekry@gmail.com>
2025-06-08 16:05:44 +0300

    modified: SkiilsProfile.tsx

--- src/features/CandidateProfile/Component/SkiilsProfile.tsx
+++ src/features/CandidateProfile/Component/SkiilsProfile.tsx
@@ -1,7 +1,6 @@
 import { MdOutlineEdit } from "react-icons/md";
 import useSkillsData from "../Hooks/useSkillsData";
 import Model from "../../../Components/Model";
-import { useGetCandidateById } from "../../OverView/Hooks/useGetCandidateById";
 import { IoAddOutline } from "react-icons/io5";
 
 const SkiilsProfile = () => {
@@ -11,16 +10,27 @@ const SkiilsProfile = () => {
     isOpenInput,
     handleOpenInput,
     register,
-    errors,
     handleSubmit,
     OnSubmit,
+    Skills,
   } = useSkillsData();
-  const { CandidateById } = useGetCandidateById();
 
   return (
     <div className="border-2 px-10 py-5 mt-5  text-white flex justify-between gap-5 rounded-xl  border-gray-500">
       <div>
         <h1 className="text-2xl font-bold">Skills</h1>
+        <div className="flex gap-5 mt-4">
+          {Skills?.result?.result?.map((skill, index) => (
+            <div
+              key={index}
+              className="flex items-center gap-4 cursor-pointer hover:bg-black hover:text-white font-bold transition-all duration-500 rounded-xl"
+            >
+              <div className="border-2 border-gray-500 w-20 p-1 flex justify-center rounded-xl gap-2">
+                <h1>{skill.Skill}</h1>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
       <div>
         <MdOutlineEdit
@@ -31,39 +41,25 @@ const SkiilsProfile = () => {
       </div>
       {isOpenSkillModel && (
         <Model Header="Edit Skills">
-          <form action="" className="mt-5" onSubmit={handleSubmit(OnSubmit)}>
+          <form action="" className="mt-5 " onSubmit={handleSubmit(OnSubmit)}>
             <p className="text-md">
               Edit, add or remove skills to your profile
             </p>
-            <p className="mt-7">
-              {CandidateById?.candidator?.result?.[0]?.Skills == null ? (
-                <>
-                  <h1 className="text-red-400 text-center font-bold">
-                    you don't have any skill
-                  </h1>
-                  {!isOpenInput && (
-                    <button
-                      className="border-2 cursor-pointer border-green-300 rounded-full w-10 h-10 flex justify-center items-center"
-                      onClick={handleOpenInput}
-                    >
-                      <IoAddOutline size={30} />
-                    </button>
-                  )}
-                </>
-              ) : (
-                <div className="flex items-center gap-4">
-                  <div className="border-2 border-gray-500 w-20 p-1 flex justify-center rounded-xl gap-2">
-                    <h1>{CandidateById?.candidator?.result?.[0]?.Skills}</h1>
+            <p className="mt-7  gap-5 grid grid-cols-7">
+              {Skills?.result?.result?.map((skill, index) => (
+                <div key={index} className="flex items-center gap-4 ">
+                  <div className="border-2 border-gray-500 hover:bg-black  cursor-pointer transition-all duration-500  w-20 p-1 flex justify-center rounded-xl gap-2">
+                    <h1>{skill.Skill}</h1>
                   </div>
-                  {!isOpenInput && (
-                    <button
-                      className="border-2 cursor-pointer border-green-300 rounded-full w-10 h-10 flex justify-center items-center"
-                      onClick={handleOpenInput}
-                    >
-                      <IoAddOutline size={30} />
-                    </button>
-                  )}
                 </div>
+              ))}
+              {!isOpenInput && (
+                <button
+                  className="border-2 cursor-pointer border-green-300 rounded-full w-10 h-10 flex justify-center items-center"
+                  onClick={handleOpenInput}
+                >
+                  <IoAddOutline size={30} />
+                </button>
               )}
             </p>
             {isOpenInput && (
@@ -72,7 +68,7 @@ const SkiilsProfile = () => {
                   type="text"
                   placeholder="Type to search"
                   className="w-full py-3 border-2 border-gray-500 px-2 rounded-xl"
-                  {...register("Skills", { required: "Skill is required" })}
+                  {...register("Skill", { required: "Skill is required" })}
                 />
               </p>
             )}
