540cc63ecfe039f854c0141286c241a4e3291592
moshaosama <mohamedOSFekry@gmail.com>
2025-06-08 19:17:57 +0300

    added: JobsCandidatePage Actions

---
+++ src/features/JobsCandidatePage/Actions/ApplyJob.ts
@@ -0,0 +1,16 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { applyJobService } from "../../../APi/ApplyJob/applyJobService";
+
+export const fetchApplyJob = createAsyncThunk(
+  "ApplyJob/fetchApplyJob",
+  (data: any, thunkAPi: any) => {
+    return applyJobService.createNewData(data, thunkAPi);
+  }
+);
+
+export const fetchGetApplyJobs = createAsyncThunk(
+  "ApplyJob/fetchGetApplyJobs",
+  (candidator_id: any, thunkAPi: any) => {
+    return applyJobService.getAllData(candidator_id, thunkAPi);
+  }
+);
