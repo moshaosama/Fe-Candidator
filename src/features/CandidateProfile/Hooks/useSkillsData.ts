a0cc52890b16b27afeeecfdd34a0cf1e90ddfa90
moshaosama <mohamedOSFekry@gmail.com>
2025-06-08 19:17:50 +0300

    modified: useSkillsData.ts

--- src/features/CandidateProfile/Hooks/useSkillsData.ts
+++ src/features/CandidateProfile/Hooks/useSkillsData.ts
@@ -8,7 +8,7 @@ import { fetchCreateSkill } from "../Actions/CreateSkill";
 import { SkillData } from "../../../Types/CreateSkill";
 
 const useSkillsData = () => {
-  const [isOpenSkillModel, setIsOpenSkillModel] = useState(true);
+  const [isOpenSkillModel, setIsOpenSkillModel] = useState(false);
   const [isOpenInput, setIsOpenInput] = useState(false);
   const Skills = useSelector((state: RootState) => state.Skill) as SkillData;
   const dispatch = useDispatch<AppDispatch>();
