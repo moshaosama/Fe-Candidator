192cd7661941ebc721b245f22025f2020535e2ac
moshaosama <mohamedOSFekry@gmail.com>
2025-06-16 04:02:40 +0300

    Latest Version

--- src/features/Dashboard/Components/ChartDiagram.tsx
+++ src/features/Dashboard/Components/ChartDiagram.tsx
@@ -6,14 +6,29 @@ const ChartDiagram = () => {
 
   return (
     <div className="flex mt-14 gap-4">
-      <div className="border-2 hover:scale-105 transition-all duration-700 border-gray-800 shadow-lg shadow-black w-1/2 bg-[#1e1e1e] p-6 rounded-lg">
+      <div className="border-2 text-white hover:scale-105 transition-all duration-700 border-gray-800 shadow-lg shadow-black w-1/2 bg-[#1e1e1e] p-6 rounded-lg">
         <h1 className="text-white font-bold text-2xl mb-4">Company Growth</h1>
         <BarChart
           dataset={dataset}
-          xAxis={[{ dataKey: "month" }]}
+          xAxis={[
+            {
+              dataKey: "month",
+              label: "Month",
+              tickLabelStyle: {
+                fill: "white",
+              },
+            },
+          ]}
+          yAxis={[
+            {
+              tickLabelStyle: {
+                fill: "white",
+              },
+            },
+          ]}
           series={[{ dataKey: "count", label: "Company Count" }]}
           height={200}
-          colors={["#ad46ff"]}
+          colors={["red"]}
         />
       </div>
     </div>
