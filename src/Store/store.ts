f3a6808a898d4752981f65201bdcb886111eeb32
moshaosama <mohamedOSFekry@gmail.com>
2025-06-18 17:37:33 +0300

    modified: registered new job-related reducers in store

--- src/Store/store.ts
+++ src/Store/store.ts
@@ -5,6 +5,7 @@ import ContactReducer from "../features/Contact/Reducer/ContactReducer";
 import {
   jobByIDReducer,
   jobReducer,
+  jobByCompanyIdReducer,
 } from "../features/Jobs/Reducer/JobReducer";
 import StagesReducer from "../features/Stages/Reducer/StageSlice";
 import CandidatorReducer from "../features/Candidates/Reducer/Candidator";
@@ -20,6 +21,7 @@ export const store = configureStore({
     contact: ContactReducer,
     job: jobReducer,
     jobByid: jobByIDReducer,
+    jobByCompanyId: jobByCompanyIdReducer,
     candidator: CandidatorReducer.Candidator,
     candidatorByStage: CandidatorReducer.CandidateByStage,
     candidatorById: CandidatorReducer.CandidateById,
