7f2b90b61ddaf146a9ff466a1838bdb67de6316c
moshaosama <mohamedOSFekry@gmail.com>
2025-06-18 19:31:20 +0300

    added: Settings page layout and structure

---
+++ src/Pages/Settings_/index.tsx
@@ -0,0 +1,11 @@
+import Settings from "../../features/Settings_/Components/Settings";
+
+const SettingPage = () => {
+  return (
+    <>
+      <Settings />
+    </>
+  );
+};
+
+export default SettingPage;
