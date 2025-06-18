7025d48583828f9e8252370d950d54934f3fc32f
moshaosama <mohamedOSFekry@gmail.com>
2025-06-18 17:37:35 +0300

    modified: improved job display component to handle company-specific jobs

--- src/features/Jobs/Components/DisplayData.tsx
+++ src/features/Jobs/Components/DisplayData.tsx
@@ -1,49 +1,33 @@
 import { Link } from "react-router";
-import { Company } from "../../Companies/Types/CompanyType";
-import UseGetCompanyByID from "../Hooks/UseGetCompanyByID";
-
-import { JobData } from "../Types/JobData";
+import useGetJobByCompanyId from "../Hooks/useGetJobByCompanyId";
 
 const DisplayData = () => {
-  const { CompanyByID } = UseGetCompanyByID();
+  const { jobByCompanyId } = useGetJobByCompanyId();
 
   return (
     <>
-      {CompanyByID.company.result?.map((company: Company) => {
-        let jobs: JobData[] = [];
-
-        if (typeof company.Jobs === "string") {
-          try {
-            jobs = JSON.parse(company.Jobs);
-          } catch (err) {
-            console.error("خطأ في تحويل Jobs:", err);
-            return null;
-          }
-        }
-
+      {jobByCompanyId.jobs.result?.map((job: any) => {
         return (
-          <div key={company.id}>
-            {jobs.map((job: JobData) => (
-              <Link to={`jobs/${job.id}`}>
-                <div
-                  key={job.id}
-                  className="p-4 bg-[#272727ab] rounded-lg text-white font-semibold max-sm:text-[10px] cursor-pointer mt-3"
-                >
-                  <div className="grid grid-cols-10  text-white font-semibold max-sm:text-[10px]">
-                    <p>{job.JobTitle}</p>
-                    <p>{job.Description}</p>
-                    <p>{job.Department}</p>
-                    <p>{job.NumNeeded}</p>
-                    <p>-</p>
-                    <p>{job.Location}</p>
-                    <p>-</p>
-                    <p>{job.Commitment}</p>
-                    <p>-</p>
-                    <p>{job.Contact}</p>
-                  </div>
+          <div key={job.id}>
+            <Link to={`jobs/${job.id}`}>
+              <div
+                key={job.id}
+                className="p-4 bg-[#272727ab] rounded-lg text-white font-semibold max-sm:text-[10px] cursor-pointer mt-3"
+              >
+                <div className="grid grid-cols-10  text-white font-semibold max-sm:text-[10px]">
+                  <p>{job.JobTitle}</p>
+                  <p>{job.Description}</p>
+                  <p>{job.Department}</p>
+                  <p>{job.NumNeeded}</p>
+                  <p>-</p>
+                  <p>{job.Location}</p>
+                  <p>-</p>
+                  <p>{job.Commitment}</p>
+                  <p>-</p>
+                  <p>{job.Contact}</p>
                 </div>
-              </Link>
-            ))}
+              </div>
+            </Link>
           </div>
         );
       })}
