818efb8aa02a20d24dbc2bcda220ff5bc4470eb3
moshaosama <mohamedOSFekry@gmail.com>
2025-06-19 15:00:49 +0300

    added: AccountSettings page structure

---
+++ src/Pages/AccountSettings/index.tsx
@@ -0,0 +1,11 @@
+import AccountDetails from "../../features/Settings_/Components/AccountDetails";
+
+const Account_Settngs = () => {
+  return (
+    <>
+      <AccountDetails />
+    </>
+  );
+};
+
+export default Account_Settngs;
