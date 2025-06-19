3c2b7e86e177e3eaf88a62010e8c2724036b4238
moshaosama <mohamedOSFekry@gmail.com>
2025-06-19 14:12:59 +0300

    modified: adjusted layout and props in CardDashboardData component

--- src/Components/CardDashboardData.tsx
+++ src/Components/CardDashboardData.tsx
@@ -1,11 +1,21 @@
+import clsx from "clsx";
 import { CardDashboardProps } from "../Types/CardDashboardData";
+import { useToggleTheme } from "../Context/useToggleThemeContext";
 
 export const CardDashboardData = (props: CardDashboardProps) => {
+  const { Theme } = useToggleTheme();
   return (
     <>
-      <div className="bg-gradient-to-l to-purple-700 from-50% shadow-lg shadow-black hover:scale-105 transition-all duration-700 flex flex-col gap-5 border-gray-800 border-2  rounded-xl p-2 mt-5">
-        <h1 className="text-white  text-2xl">{props.Title}</h1>
-        <p className="text-white font-bold text-2xl">{props.numofData}</p>
+      <div
+        className={clsx(
+          " shadow-lg shadow-black hover:scale-105 transition-all duration-700 flex flex-col gap-5 border-gray-800 border-2  rounded-xl p-2 mt-5",
+          Theme === "dark"
+            ? "text-white bg-gradient-to-l to-purple-700 from-50%"
+            : "text-white bg-black"
+        )}
+      >
+        <h1 className={clsx(" text-2xl")}>{props.Title}</h1>
+        <p className="font-bold text-2xl">{props.numofData}</p>
       </div>
     </>
   );
