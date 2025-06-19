581b256608a32ec2010d859f38e035f243bc49c2
moshaosama <mohamedOSFekry@gmail.com>
2025-06-19 14:12:59 +0300

    modified: updated table and props handling in Companies display

--- src/features/Companies/Components/DisplayData.tsx
+++ src/features/Companies/Components/DisplayData.tsx
@@ -1,17 +1,24 @@
 import { Link } from "react-router";
 import useGetCompanyData from "../Hooks/useGetCompanyData";
+import clsx from "clsx";
+import { useToggleTheme } from "../../../Context/useToggleThemeContext";
 
 const DisplayData = () => {
   const { Company } = useGetCompanyData();
-
+  const { Theme } = useToggleTheme();
   return (
     <>
       {Company?.company.result?.map((Company, index) => (
         <Link key={index} to={`/companies/${Company.id}`}>
-          <div className="bg-[#272727ab] mt-3 py-5 px-4 rounded-lg cursor-pointer">
+          <div
+            className={clsx(
+              " mt-3 py-5 px-4 rounded-lg cursor-pointer",
+              Theme == "dark" ? "bg-[#272727ab] text-white" : "bg-[#a5a4a4ab]"
+            )}
+          >
             <div
               key={index}
-              className="grid grid-cols-4 text-white font-semibold max-sm:text-[10px]"
+              className="grid grid-cols-4  font-semibold max-sm:text-[10px]"
             >
               <h1>{Company.Name}</h1>
               <h1>{Company.Language}</h1>
