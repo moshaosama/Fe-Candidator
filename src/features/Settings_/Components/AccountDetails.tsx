d7d124efa05e196052ad54939a4f055f7288b0a0
moshaosama <mohamedOSFekry@gmail.com>
2025-06-20 20:18:14 +0300

    modified: refined AccountDetails inputs and error handling

--- src/features/Settings_/Components/AccountDetails.tsx
+++ src/features/Settings_/Components/AccountDetails.tsx
@@ -8,7 +8,7 @@ const AccountDetails = () => {
         <h1 className="text-white font-bold text-3xl mt-10 ml-10 ">
           Account Settings
         </h1>
-        <div className="flex gap-5">
+        <div className="flex gap-5 max-sm:flex-col">
           <SidebarSetting />
           <div className="mt-10">
             <Outlet />
