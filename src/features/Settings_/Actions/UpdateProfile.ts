b7162dc01a51ee3867142aa31583dbb790208b22
moshaosama <mohamedOSFekry@gmail.com>
2025-06-20 19:40:51 +0300

    modified: refined update logic in UpdateProfile action

--- src/features/Settings_/Actions/UpdateProfile.ts
+++ src/features/Settings_/Actions/UpdateProfile.ts
@@ -1,5 +1,6 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import {
+  ChangePasswordData,
   changeProfileService,
   UpdateProfileData,
 } from "../../../APi/ChangeProfile/ChangeProfileService";
@@ -8,5 +9,12 @@ export const fetchUpdateProfile = createAsyncThunk(
   "UpdateProfile/fetchUpdateProfile",
   (data: UpdateProfileData, thunkAPI: any) => {
     return changeProfileService.UpdateProfile(data, thunkAPI);
+  }
+);
+
+export const fetchChangePassword = createAsyncThunk(
+  "UpdateProfile/fetchChangePassword",
+  (data: ChangePasswordData, thunkAPI: any) => {
+    return changeProfileService.ChangePassword(data, thunkAPI);
   }
 );
