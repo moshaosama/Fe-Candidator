5e01ab90fbc16510fa727ab187f00a44de84a996
moshaosama <mohamedOSFekry@gmail.com>
2025-06-20 20:18:13 +0300

    modified: updated ChartDiagram to reflect new data structure

--- src/features/Dashboard/Components/ChartDiagram.tsx
+++ src/features/Dashboard/Components/ChartDiagram.tsx
@@ -5,8 +5,8 @@ const ChartDiagram = () => {
   const { dataset } = ueGetCompany();
 
   return (
-    <div className="flex mt-14 gap-4">
-      <div className="border-2 text-white hover:scale-105 transition-all duration-700 border-gray-800 shadow-lg shadow-black w-1/2 bg-[#1e1e1e] p-6 rounded-lg">
+    <div className="flex mt-14 max-sm:mx-5 gap-4">
+      <div className="border-2 max-sm:w-full text-white hover:scale-105 transition-all duration-700 border-gray-800 shadow-lg shadow-black w-1/2 bg-[#1e1e1e] p-6 rounded-lg">
         <h1 className="text-white font-bold text-2xl mb-4">Company Growth</h1>
         <BarChart
           dataset={dataset}
