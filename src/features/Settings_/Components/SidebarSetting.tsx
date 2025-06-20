243973143476c979ee7bc0e0392860288092654b
moshaosama <mohamedOSFekry@gmail.com>
2025-06-20 20:06:54 +0300

    modified: improved layout and active state logic in SidebarSetting

--- src/features/Settings_/Components/SidebarSetting.tsx
+++ src/features/Settings_/Components/SidebarSetting.tsx
@@ -2,16 +2,19 @@ import { IoMdPerson } from "react-icons/io";
 import { RiLockPasswordLine } from "react-icons/ri";
 import useSetActive from "../Hooks/useSetActive";
 import { Link } from "react-router";
+import { CiLogout } from "react-icons/ci";
+import useLogout from "../../../Hooks/useLogout";
 
 const SidebarSetting = () => {
   const { handleClick } = useSetActive();
+  const { handleLogout } = useLogout();
   return (
     <>
       <div className="mx-10 mt-10 h-fit bg-white w-60 rounded-xl shadow-2xl shadow-black">
         <Link to={"/settings/account-setting"}>
           <div
             onClick={handleClick}
-            className="flex items-center p-3 hover:bg-black hover:text-white transition-all duration-500 cursor-pointer hover:rounded-t-lg Active"
+            className="flex gap-5 items-center p-3 hover:bg-black hover:text-white transition-all duration-500 cursor-pointer hover:rounded-t-lg Active"
           >
             <IoMdPerson />
             <h1>Profile Settings</h1>
@@ -20,12 +23,19 @@ const SidebarSetting = () => {
         <Link to={"change-password"}>
           <div
             onClick={handleClick}
-            className="flex items-center p-3 hover:bg-black hover:text-white transition-all duration-500 cursor-pointer hover:rounded-b-lg"
+            className="flex gap-5 items-center p-3 hover:bg-black hover:text-white transition-all duration-500 cursor-pointer hover:rounded-b-lg"
           >
             <RiLockPasswordLine />
             <h1>Password</h1>
           </div>
         </Link>
+        <div
+          onClick={handleLogout}
+          className="flex gap-5 items-center p-3 hover:bg-black hover:text-white transition-all duration-500 cursor-pointer hover:rounded-b-lg"
+        >
+          <CiLogout />
+          <h1>Logout</h1>
+        </div>
       </div>
     </>
   );
