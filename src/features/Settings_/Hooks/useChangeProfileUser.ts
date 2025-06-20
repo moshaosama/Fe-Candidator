1a7df95eb917b7b84bc68f8d72d346cd44a343ff
moshaosama <mohamedOSFekry@gmail.com>
2025-06-20 19:40:52 +0300

    modified: optimized hook logic in useChangeProfileUser

--- src/features/Settings_/Hooks/useChangeProfileUser.ts
+++ src/features/Settings_/Hooks/useChangeProfileUser.ts
@@ -1,18 +1,29 @@
 import { useForm } from "react-hook-form";
 import { useDispatch } from "react-redux";
 import { AppDispatch } from "../../../Store/store";
-import { fetchUpdateProfile } from "../Actions/UpdateProfile";
+import {
+  fetchChangePassword,
+  fetchUpdateProfile,
+} from "../Actions/UpdateProfile";
 import { useGetToken } from "../../../Hooks/useGetToken";
 
 const useChangeProfileUser = () => {
   const { User } = useGetToken();
-  const { register, handleSubmit } = useForm({
+  const {
+    register: registerChangeProfile,
+    handleSubmit: handleSubmitChangeProfile,
+  } = useForm({
     defaultValues: {
       FirstName: User?.result?.FirstName,
       LastName: User?.result?.LastName,
       Email: User?.result?.Email,
     },
   });
+
+  const {
+    register: registerChangePassword,
+    handleSubmit: handleSubmitChangePassword,
+  } = useForm();
   const dispatch = useDispatch<AppDispatch>();
   const handleChangeProfileUser = (data: any) => {
     dispatch(fetchUpdateProfile(data));
@@ -21,7 +32,21 @@ const useChangeProfileUser = () => {
     window.location.reload();
   };
 
-  return { register, handleSubmit, handleChangeProfileUser };
+  const handleChangePassword = (data: any) => {
+    dispatch(fetchChangePassword(data));
+    localStorage.removeItem("User");
+    localStorage.removeItem("Token");
+    window.location.reload();
+  };
+
+  return {
+    registerChangeProfile,
+    handleSubmitChangeProfile,
+    handleChangeProfileUser,
+    handleChangePassword,
+    registerChangePassword,
+    handleSubmitChangePassword,
+  };
 };
 
 export default useChangeProfileUser;
