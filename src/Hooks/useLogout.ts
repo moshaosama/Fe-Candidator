6f7c262abafd6e4893f2c6b9f5c008bce5f78b29
moshaosama <mohamedOSFekry@gmail.com>
2025-06-20 20:06:54 +0300

    added: global useLogout hook for handling user logout

---
+++ src/Hooks/useLogout.ts
@@ -0,0 +1,11 @@
+const useLogout = () => {
+  const handleLogout = () => {
+    window.localStorage.removeItem("User");
+    window.localStorage.removeItem("Token");
+    window.location.reload();
+  };
+
+  return { handleLogout };
+};
+
+export default useLogout;
